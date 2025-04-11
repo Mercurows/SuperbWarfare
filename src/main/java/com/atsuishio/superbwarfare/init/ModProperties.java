9bd14bf824759f38c299a67a2f782b2352e1e881
17146 <1714673995@qq.com>
2025-04-11 20:32:40 +0800

    注册物品属性覆盖

--- src/main/java/com/atsuishio/superbwarfare/init/ModProperties.java
+++ src/main/java/com/atsuishio/superbwarfare/init/ModProperties.java
@@ -1,5 +1,8 @@
 package com.atsuishio.superbwarfare.init;
 
+import com.atsuishio.superbwarfare.Mod;
+import com.atsuishio.superbwarfare.tools.NBTTool;
+import net.minecraft.client.renderer.item.ItemProperties;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -7,11 +10,12 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModProperties {
+
     @SubscribeEvent
     public static void propertyOverrideRegistry(FMLClientSetupEvent event) {
-//        event.enqueueWork(() -> ItemProperties.register(ModItems.MONITOR.get(), ModUtils.loc("monitor_linked"),
-//                (itemStack, clientWorld, livingEntity, seed) -> GunNBTTool.getBoolean(itemStack, "Linked", false) ? 1.0F : 0.0F));
-//        event.enqueueWork(() -> ItemProperties.register(ModItems.ARMOR_PLATE.get(), ModUtils.loc("armor_plate_infinite"),
-//                (itemStack, clientWorld, livingEntity, seed) -> GunNBTTool.getBoolean(itemStack, "Infinite", false) ? 1.0F : 0.0F));
+        event.enqueueWork(() -> ItemProperties.register(ModItems.MONITOR.get(), Mod.loc("monitor_linked"),
+                (itemStack, clientWorld, livingEntity, seed) -> NBTTool.getTag(itemStack).getBoolean("Linked") ? 1.0F : 0.0F));
+        event.enqueueWork(() -> ItemProperties.register(ModItems.ARMOR_PLATE.get(), Mod.loc("armor_plate_infinite"),
+                (itemStack, clientWorld, livingEntity, seed) -> NBTTool.getTag(itemStack).getBoolean("Infinite") ? 1.0F : 0.0F));
     }
 }
