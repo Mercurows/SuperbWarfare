c21662b1c00226011c936d9e881e737ca7926b06
Atsuishio <842960157@qq.com>
2025-05-13 15:07:28 +0800

    修复飞机HUD的一个bug，微调A10操控

--- src/main/java/com/atsuishio/superbwarfare/event/ClientMouseHandler.java
+++ src/main/java/com/atsuishio/superbwarfare/event/ClientMouseHandler.java
@@ -170,8 +170,12 @@ public class ClientMouseHandler {
             return 0.33;
         }
 
-        if (player.getVehicle() instanceof Tom6Entity || player.getVehicle() instanceof A10Entity) {
+        if (player.getVehicle() instanceof Tom6Entity) {
             return 0.3;
+        }
+
+        if (player.getVehicle() instanceof A10Entity) {
+            return 0.25;
         }
 
         return original;
