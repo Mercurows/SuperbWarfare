9f34ec110a67a05f39be8ab16416545051c100f8
17146 <1714673995@qq.com>
2025-05-22 01:41:30 +0800

    规范化rpk

--- src/main/java/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.java
+++ src/main/java/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.java
@@ -55,6 +55,7 @@ public class ModItemModelProvider extends ItemModelProvider {
         gunItem(ModItems.NTW_20);
         gunItem(ModItems.QBZ_95);
         gunItem(ModItems.RPG);
+        gunItem(ModItems.RPK);
 
         simpleItem(ModItems.EMPTY_PERK, "perk/");
 
