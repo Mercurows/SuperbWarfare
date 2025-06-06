6b568fb006c093c4dd4b95b4b089c36191cb5090
17146 <1714673995@qq.com>
2025-06-06 21:32:01 +0800

    允许关闭自动防御塔

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/LaserTowerEntity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/LaserTowerEntity.java
@@ -142,9 +142,13 @@ public class LaserTowerEntity extends EnergyVehicleEntity implements GeoEntity, 
                 this.remove(RemovalReason.DISCARDED);
                 this.discard();
                 return InteractionResult.SUCCESS;
-            } else if (!entityData.get(ACTIVE)) {
-                entityData.set(ACTIVE, true);
-                this.setOwnerUUID(player.getUUID());
+            } else {
+                if (this.getOwnerUUID() == null) {
+                    this.setOwnerUUID(player.getUUID());
+                }
+
+                entityData.set(ACTIVE, !entityData.get(ACTIVE));
+
                 if (player instanceof ServerPlayer serverPlayer) {
                     serverPlayer.level().playSound(null, serverPlayer.getOnPos(), SoundEvents.ARROW_HIT_PLAYER, SoundSource.PLAYERS, 0.5F, 1);
                 }
