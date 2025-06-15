4498b715732c7a8b29edbe121a0aae85fefc6bdf
Atsuishio <842960157@qq.com>
2025-06-16 01:02:57 +0800

    平滑OBB框的渲染

--- src/main/java/com/atsuishio/superbwarfare/mixins/EntityRenderDispatcherMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/EntityRenderDispatcherMixin.java
@@ -5,6 +5,7 @@ import com.atsuishio.superbwarfare.entity.OBBEntity;
 import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,7 +20,7 @@ public class EntityRenderDispatcherMixin {
             at = @At("RETURN"))
     private static void renderHitbox(PoseStack poseStack, VertexConsumer buffer, Entity p_entity, float red, float green, float blue, float alpha, CallbackInfo ci) {
         if (p_entity instanceof OBBEntity obbEntity && p_entity instanceof VehicleEntity vehicle) {
-            OBBRenderer.INSTANCE.render(vehicle, obbEntity.getOBBs(), poseStack, buffer, 0, 1, 0, 1);
+            OBBRenderer.INSTANCE.render(vehicle, obbEntity.getOBBs(), poseStack, buffer, 0, 1, 0, 1, Minecraft.getInstance().getTimer().getRealtimeDeltaTicks());
         }
     }
 }
