253c2ab66f59a6d495366f948db4c7577ea72554
Atsuishio <842960157@qq.com>
2025-06-28 02:30:48 +0800

    尝试抄tacz的弹孔，但是不显示（恼

---
+++ src/main/java/com/atsuishio/superbwarfare/client/particle/BulletDecalOption.java
@@ -0,0 +1,75 @@
+package com.atsuishio.superbwarfare.client.particle;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.tacz.guns.init.ModParticles;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BulletDecalOption implements ParticleOptions {
+    public static final Codec<BulletDecalOption> CODEC = RecordCodecBuilder.create(builder ->
+            builder.group(Codec.INT.fieldOf("dir").forGetter(option -> option.direction.ordinal()),
+                    Codec.LONG.fieldOf("pos").forGetter(option -> option.pos.asLong())
+            ).apply(builder, BulletDecalOption::new));
+
+    @SuppressWarnings("deprecation")
+    public static final ParticleOptions.Deserializer<BulletDecalOption> DESERIALIZER = new ParticleOptions.Deserializer<>() {
+        @Override
+        public BulletDecalOption fromCommand(ParticleType<BulletDecalOption> particleType, StringReader reader) throws CommandSyntaxException {
+            reader.expect(' ');
+            int dir = reader.readInt();
+            reader.expect(' ');
+            long pos = reader.readLong();
+            return new BulletDecalOption(dir, pos);
+        }
+
+        @Override
+        public BulletDecalOption fromNetwork(ParticleType<BulletDecalOption> particleType, FriendlyByteBuf buffer) {
+            return new BulletDecalOption(buffer.readVarInt(), buffer.readLong());
+        }
+    };
+
+    private final Direction direction;
+    private final BlockPos pos;
+
+    public BulletDecalOption(int dir, long pos) {
+        this.direction = Direction.values()[dir];
+        this.pos = BlockPos.of(pos);
+
+    }
+
+    public BulletDecalOption(Direction dir, BlockPos pos) {
+        this.direction = dir;
+        this.pos = pos;
+    }
+
+    public Direction getDirection() {
+        return this.direction;
+    }
+
+    public BlockPos getPos() {
+        return this.pos;
+    }
+
+    @Override
+    public ParticleType<?> getType() {
+        return ModParticles.BULLET_HOLE.get();
+    }
+
+    @Override
+    public void writeToNetwork(FriendlyByteBuf buffer) {
+        buffer.writeEnum(this.direction);
+        buffer.writeBlockPos(this.pos);
+    }
+
+    @Override
+    public String writeToString() {
+        return ForgeRegistries.PARTICLE_TYPES.getKey(this.getType()) + " " + this.direction.getName();
+    }
+}
--- src/main/java/com/atsuishio/superbwarfare/init/ModParticleTypes.java
+++ src/main/java/com/atsuishio/superbwarfare/init/ModParticleTypes.java
@@ -1,18 +1,38 @@
 package com.atsuishio.superbwarfare.init;
 
 import com.atsuishio.superbwarfare.Mod;
+import com.atsuishio.superbwarfare.client.particle.BulletDecalOption;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.NotNull;
 
 public class ModParticleTypes {
     public static final DeferredRegister<ParticleType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, Mod.MODID);
 
+
     public static final RegistryObject<SimpleParticleType> FIRE_STAR = REGISTRY.register("fire_star", () -> new SimpleParticleType(false));
-    public static final RegistryObject<SimpleParticleType> BULLET_HOLE = REGISTRY.register("bullet_hole", () -> new SimpleParticleType(false));
+    public static final RegistryObject<ParticleType<BulletDecalOption>> BULLET_DECAL = REGISTRY.register("bullet_decal", () -> new ModParticleType<>(false, BulletDecalOption.DESERIALIZER, BulletDecalOption.CODEC));
     public static final RegistryObject<SimpleParticleType> CUSTOM_CLOUD = REGISTRY.register("custom_cloud", () -> new SimpleParticleType(false));
     public static final RegistryObject<SimpleParticleType> CUSTOM_SMOKE = REGISTRY.register("custom_smoke", () -> new SimpleParticleType(false));
+
+    @SuppressWarnings("deprecation")
+    private static class ModParticleType<T extends ParticleOptions> extends ParticleType<T> {
+        private final Codec<T> codec;
+
+        public ModParticleType(boolean overrideLimiter, ParticleOptions.Deserializer<T> deserializer, Codec<T> codec) {
+            super(overrideLimiter, deserializer);
+            this.codec = codec;
+        }
+
+        @Override
+        public @NotNull Codec<T> codec() {
+            return this.codec;
+        }
+    }
 }
 
