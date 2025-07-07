063c52b9933ebce5599a2dc3840b1381fd3844da
17146 <1714673995@qq.com>
2025-07-08 00:15:03 +0800

    先添加一个screen

---
+++ src/main/java/com/atsuishio/superbwarfare/client/screens/FiringParametersScreen.java
@@ -0,0 +1,20 @@
+package com.atsuishio.superbwarfare.client.screens;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+// TODO 完成这个screen
+@OnlyIn(Dist.CLIENT)
+public class FiringParametersScreen extends Screen {
+
+    public static final Component TITLE = Component.translatable("item.superbwarfare.firing_parameters");
+    private final ItemStack stack;
+
+    protected FiringParametersScreen(ItemStack stack) {
+        super(TITLE);
+        this.stack = stack;
+    }
+}
