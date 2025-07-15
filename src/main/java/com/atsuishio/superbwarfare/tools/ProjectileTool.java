8c00ec72f4d0bf28f878eb6d63535295aead6a95
Atsuishio <842960157@qq.com>
2025-07-15 12:52:33 +0800

    优化音效

--- src/main/java/com/atsuishio/superbwarfare/tools/ProjectileTool.java
+++ src/main/java/com/atsuishio/superbwarfare/tools/ProjectileTool.java
@@ -25,9 +25,9 @@ public class ProjectileTool {
         EventHooks.onExplosionStart(projectile.level(), explosion);
         explosion.finalizeExplosion(false);
 
-        if (radius <= 5) {
+        if (radius <= 4) {
             ParticleTool.spawnSmallExplosionParticles(projectile.level(), projectile.position().add(projectile.getDeltaMovement().scale(0.5)));
-        } else if (radius > 5 && radius < 10) {
+        } else if (radius > 4 && radius < 10) {
             ParticleTool.spawnMediumExplosionParticles(projectile.level(), projectile.position().add(projectile.getDeltaMovement().scale(0.5)));
         } else {
             ParticleTool.spawnHugeExplosionParticles(projectile.level(), projectile.position().add(projectile.getDeltaMovement().scale(0.5)));
