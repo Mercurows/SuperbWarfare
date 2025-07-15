48c486619526d4139dd928df1e615aec247d4bad
17146 <1714673995@qq.com>
2025-07-16 01:23:31 +0800

    修复63式无法发射的问题

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Type63Entity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Type63Entity.java
@@ -233,7 +233,7 @@ public class Type63Entity extends ContainerMobileVehicleEntity implements GeoEnt
                 if (lookingAtBarrel(player)) {
                     // 精准发射
                     for (int i = 0; i < this.barrel.length; i++) {
-                        if (OBB.getLookingObb(player, player.getEntityReach()) == this.barrel[i] && items.get(i).getItem() instanceof MediumRocketItem && cooldown == 0 && getEnergy() > 0) {
+                        if (OBB.getLookingObb(player, player.getEntityReach()) == this.barrel[i] && items.get(i).getItem() instanceof MediumRocketItem && cooldown == 0) {
                             shoot(player, i);
                             items.set(i, ItemStack.EMPTY);
                             setChanged();
@@ -244,7 +244,7 @@ public class Type63Entity extends ContainerMobileVehicleEntity implements GeoEnt
                 } else {
                     // 顺序发射
                     for (int i = 0; i < 12; i++) {
-                        if (items.get(i).getItem() instanceof MediumRocketItem && cooldown == 0 && getEnergy() > 0) {
+                        if (items.get(i).getItem() instanceof MediumRocketItem && cooldown == 0) {
                             shoot(player, i);
                             items.set(i, ItemStack.EMPTY);
                             setChanged();
@@ -320,7 +320,6 @@ public class Type63Entity extends ContainerMobileVehicleEntity implements GeoEnt
             }
         }
 
-        consumeEnergy(1);
         ShakeClientMessage.sendToNearbyPlayers(this, 8, 8, 10, 20);
     }
 
