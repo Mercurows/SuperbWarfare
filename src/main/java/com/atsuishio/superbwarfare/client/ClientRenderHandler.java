e08f5fd5684cf448c7ff8050897573adb4099b41
17146 <1714673995@qq.com>
2025-07-22 18:23:14 +0800

    尝试添加curio渲染

--- src/main/java/com/atsuishio/superbwarfare/client/ClientRenderHandler.java
+++ src/main/java/com/atsuishio/superbwarfare/client/ClientRenderHandler.java
@@ -3,6 +3,7 @@ package com.atsuishio.superbwarfare.client;
 import com.atsuishio.superbwarfare.client.decorator.ContainerItemDecorator;
 import com.atsuishio.superbwarfare.client.decorator.LuckyContainerItemDecorator;
 import com.atsuishio.superbwarfare.client.overlay.*;
+import com.atsuishio.superbwarfare.client.renderer.armor.GeHelmetM35ArmorRenderer;
 import com.atsuishio.superbwarfare.client.renderer.block.*;
 import com.atsuishio.superbwarfare.client.tooltip.*;
 import com.atsuishio.superbwarfare.client.tooltip.component.*;
@@ -11,10 +12,12 @@ import com.atsuishio.superbwarfare.init.ModItems;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterItemDecorationsEvent;
+import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientRenderHandler {
@@ -72,5 +75,10 @@ public class ClientRenderHandler {
     public static void registerItemDecorations(RegisterItemDecorationsEvent event) {
         event.register(ModItems.CONTAINER.get(), new ContainerItemDecorator());
         event.register(ModItems.LUCKY_CONTAINER.get(), new LuckyContainerItemDecorator());
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        CuriosRendererRegistry.register(ModItems.GE_HELMET_M_35.get(), GeHelmetM35ArmorRenderer::new);
     }
 }
--- src/main/java/com/atsuishio/superbwarfare/client/renderer/armor/GeHelmetM35ArmorRenderer.java
+++ src/main/java/com/atsuishio/superbwarfare/client/renderer/armor/GeHelmetM35ArmorRenderer.java
@@ -2,22 +2,50 @@ package com.atsuishio.superbwarfare.client.renderer.armor;
 
 import com.atsuishio.superbwarfare.client.model.armor.GeHelmetM35Model;
 import com.atsuishio.superbwarfare.item.armor.GeHelmetM35;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.client.ICurioRenderer;
 
-public class GeHelmetM35ArmorRenderer extends GeoArmorRenderer<GeHelmetM35> {
+public class GeHelmetM35ArmorRenderer extends GeoArmorRenderer<GeHelmetM35> implements ICurioRenderer {
     public GeHelmetM35ArmorRenderer() {
         super(new GeHelmetM35Model());
-        this.head = new GeoBone(null, "", false, (double) 0, false, false);
+        this.head = new GeoBone(null, "", false, 0.0, false, false);
     }
-
-    //TODO 尝试让戴在饰品栏里时让头盔渲染
 
     @Override
     public RenderType getRenderType(GeHelmetM35 animatable, ResourceLocation texture, MultiBufferSource bufferSource, float partialTick) {
         return RenderType.entityTranslucent(getTextureLocation(animatable));
+    }
+
+    // TODO 让头盔正确跟着头旋转
+    @Override
+    public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack matrixStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        matrixStack.pushPose();
+
+        var clientExtensions = IClientItemExtensions.of(stack);
+        HumanoidModel<?> helmetModel = clientExtensions.getHumanoidArmorModel(slotContext.entity(), stack, EquipmentSlot.HEAD, null);
+
+        this.prepForRender(slotContext.entity(), stack, EquipmentSlot.HEAD, helmetModel, renderTypeBuffer, partialTicks, limbSwing, limbSwingAmount, netHeadYaw, headPitch);
+
+        applyBaseModel(this.baseModel);
+        grabRelevantBones(getGeoModel().getBakedModel(getGeoModel().getModelResource(this.animatable)));
+        applyBaseTransformations(this.baseModel);
+
+        this.renderToBuffer(matrixStack, renderTypeBuffer.getBuffer(RenderType.entityTranslucent(getTextureLocation((GeHelmetM35) stack.getItem()))), light, OverlayTexture.NO_OVERLAY, 0xFFFFFFFF);
+
+        matrixStack.popPose();
     }
 }
