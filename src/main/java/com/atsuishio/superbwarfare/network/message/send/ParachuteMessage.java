b4940a10f5e5ce53a9c8f9e1ced6bd3ea3bbbed3
17146 <1714673995@qq.com>
2025-07-26 21:04:25 +0800

    调整降落伞message

--- src/main/java/com/atsuishio/superbwarfare/network/message/send/ParachuteMessage.java
+++ src/main/java/com/atsuishio/superbwarfare/network/message/send/ParachuteMessage.java
@@ -2,9 +2,9 @@ package com.atsuishio.superbwarfare.network.message.send;
 
 import com.atsuishio.superbwarfare.init.ModItems;
 import com.atsuishio.superbwarfare.init.ModSounds;
+import com.atsuishio.superbwarfare.item.curio.ParachuteItem;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.network.NetworkEvent;
 import top.theillusivec4.curios.api.CuriosApi;
 
@@ -18,27 +18,26 @@ public enum ParachuteMessage {
         context.enqueueWork(() -> {
             ServerPlayer player = context.getSender();
 
-            if (player != null) {
-                Level level = player.level();
-                CuriosApi.getCuriosInventory(player).ifPresent(
-                        c -> c.findFirstCurio(ModItems.PARACHUTE.get()).ifPresent(
-                                s -> {
-                                    var stack = s.stack();
-                                    if (!player.getCooldowns().isOnCooldown(stack.getItem())) {
-                                        if (!stack.getOrCreateTag().getBoolean("open") && player.getDeltaMovement().y < -0.6) {
-                                            stack.getOrCreateTag().putBoolean("open", true);
-                                            player.getCooldowns().addCooldown(stack.getItem(), 10);
-                                            player.level().playSound(null, player.getX(), player.getY(), player.getZ(), ModSounds.PARACHUTE_OPEN.get(), SoundSource.PLAYERS, 1f, 1);
-                                        } else if (stack.getOrCreateTag().getBoolean("open")) {
-                                            stack.getOrCreateTag().putBoolean("open", false);
-                                            player.getCooldowns().addCooldown(stack.getItem(), 10);
-                                            player.level().playSound(null, player.getX(), player.getY(), player.getZ(), ModSounds.PARACHUTE_CLOSE.get(), SoundSource.PLAYERS, 1f, 1);
-                                        }
+            if (player == null) return;
+
+            CuriosApi.getCuriosInventory(player).ifPresent(
+                    c -> c.findFirstCurio(ModItems.PARACHUTE.get()).ifPresent(
+                            s -> {
+                                var stack = s.stack();
+                                if (!player.getCooldowns().isOnCooldown(stack.getItem())) {
+                                    if (!stack.getOrCreateTag().getBoolean(ParachuteItem.TAG_OPEN) && player.getDeltaMovement().y < -0.6) {
+                                        stack.getOrCreateTag().putBoolean(ParachuteItem.TAG_OPEN, true);
+                                        player.getCooldowns().addCooldown(stack.getItem(), 10);
+                                        player.level().playSound(null, player.getX(), player.getY(), player.getZ(), ModSounds.PARACHUTE_OPEN.get(), SoundSource.PLAYERS, 1f, 1);
+                                    } else if (stack.getOrCreateTag().getBoolean(ParachuteItem.TAG_OPEN)) {
+                                        stack.getOrCreateTag().putBoolean(ParachuteItem.TAG_OPEN, false);
+                                        player.getCooldowns().addCooldown(stack.getItem(), 10);
+                                        player.level().playSound(null, player.getX(), player.getY(), player.getZ(), ModSounds.PARACHUTE_CLOSE.get(), SoundSource.PLAYERS, 1f, 1);
                                     }
                                 }
-                        )
-                );
-            }
+                            }
+                    )
+            );
         });
         context.setPacketHandled(true);
     }
