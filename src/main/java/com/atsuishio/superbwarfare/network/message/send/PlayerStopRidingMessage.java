0d45cef28f0d2a0364b076baab392c210d08f1ca
Atsuishio <842960157@qq.com>
2025-07-26 11:46:44 +0800

    添加载具弹射的音效和粒子

--- src/main/java/com/atsuishio/superbwarfare/network/message/send/PlayerStopRidingMessage.java
+++ src/main/java/com/atsuishio/superbwarfare/network/message/send/PlayerStopRidingMessage.java
@@ -3,14 +3,21 @@ package com.atsuishio.superbwarfare.network.message.send;
 import com.atsuishio.superbwarfare.Mod;
 import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity;
 import com.atsuishio.superbwarfare.init.ModMobEffects;
+import com.atsuishio.superbwarfare.init.ModSounds;
 import com.atsuishio.superbwarfare.network.message.receive.ClientSetMotionMessage;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.PacketDistributor;
 
 import java.util.function.Supplier;
+
+import static com.atsuishio.superbwarfare.tools.ParticleTool.sendParticle;
 
 public class PlayerStopRidingMessage {
 
@@ -37,6 +44,15 @@ public class PlayerStopRidingMessage {
                 if (message.ejection) {
                     var vec = vehicle.getDismountMovement(player, vehicle.getTagSeatIndex(player));
                     Mod.queueServerWork(1, () -> Mod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> player), new ClientSetMotionMessage(vec)));
+                    player.level().playSound(null, player.getX(), player.getY(), player.getZ(), ModSounds.MEDIUM_ROCKET_FIRE.get(), SoundSource.PLAYERS, 4f, 1);
+                    if (player.level() instanceof ServerLevel serverLevel) {
+                        for (int p = 0; p < 8; p++) {
+                            Vec3 pPos = player.position().add(vec.scale(p * 0.5));
+                            sendParticle(serverLevel, ParticleTypes.CLOUD, pPos.x, pPos.y, pPos.z, 10, 0.5, 0.5, 0.5, 0.05, true);
+                            sendParticle(serverLevel, ParticleTypes.FLAME, pPos.x, pPos.y, pPos.z, 20, 0.5, 0.5, 0.5, 0.05, true);
+                            sendParticle(serverLevel, ParticleTypes.CAMPFIRE_COSY_SMOKE, pPos.x, pPos.y, pPos.z, 15, 0.5, 0.5, 0.5, 0.05, true);
+                        }
+                    }
                 }
                 player.stopRiding();
                 player.setJumping(false);
