0bada7ccdd487c68a914fc81ed7991649a7dfaae
17146 <1714673995@qq.com>
2025-07-30 01:28:52 +0800

    添加降落伞的第一人称模型渲染

--- src/main/java/com/atsuishio/superbwarfare/client/renderer/curio/ParachuteRenderer.java
+++ src/main/java/com/atsuishio/superbwarfare/client/renderer/curio/ParachuteRenderer.java
@@ -5,9 +5,12 @@ import com.atsuishio.superbwarfare.client.model.curio.ParachuteModel;
 import com.atsuishio.superbwarfare.item.curio.ParachuteItem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderBuffers;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
@@ -15,11 +18,16 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
+@net.minecraftforge.fml.common.Mod.EventBusSubscriber(bus = net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ParachuteRenderer implements ICurioRenderer {
 
+    private static ParachuteModel firstPersonModel;
     private static final ResourceLocation TEXTURE = Mod.loc("textures/curio/parachute.png");
 
     private final ParachuteModel model;
@@ -46,5 +54,33 @@ public class ParachuteRenderer implements ICurioRenderer {
         }
 
         matrixStack.popPose();
+    }
+
+    @SubscribeEvent
+    public static void onRenderLevelStage(RenderLevelStageEvent event) {
+        RenderBuffers buffers = Minecraft.getInstance().renderBuffers();
+        var player = Minecraft.getInstance().player;
+        if (player == null) return;
+        if (!ParachuteItem.isParachuteOpen(player)) return;
+        PoseStack stack = event.getPoseStack();
+
+        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRANSLUCENT_BLOCKS
+                && Minecraft.getInstance().options.getCameraType() == CameraType.FIRST_PERSON) {
+            stack.pushPose();
+
+            if (firstPersonModel == null) {
+                firstPersonModel = new ParachuteModel(Minecraft.getInstance().getEntityModels().bakeLayer(ParachuteModel.LAYER_LOCATION));
+            }
+
+            stack.mulPose(Axis.XP.rotationDegrees(180));
+            stack.mulPose(Axis.YP.rotationDegrees(player.getViewYRot(1f)));
+            stack.translate(0, 1.5, 0);
+
+            firstPersonModel.prepareMobModel(player, 0, 0, event.getPartialTick());
+            firstPersonModel.setupAnim(player, 0, 0, player.tickCount, 0, 0);
+            firstPersonModel.renderToBuffer(stack, buffers.bufferSource().getBuffer(RenderType.armorCutoutNoCull(TEXTURE)), 0xFFFFFF, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+
+            stack.popPose();
+        }
     }
 }
