8714ecf3ebd8d68f34271d40926fa4f2107c87e6
Light_Quanta <zch22248@foxmail.com>
2025-08-03 21:14:23 +0800

    同步CustomCloudOption写法

--- src/main/java/com/atsuishio/superbwarfare/client/particle/CustomCloudOption.java
+++ src/main/java/com/atsuishio/superbwarfare/client/particle/CustomCloudOption.java
@@ -10,16 +10,12 @@ import net.minecraft.core.particles.ParticleType;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Vector3f;
 
 public class CustomCloudOption implements ParticleOptions {
 
-    // TODO 为啥在服务端生成这个粒子会踢人
     public static final MapCodec<CustomCloudOption> CODEC = RecordCodecBuilder.mapCodec(builder ->
             builder.group(
-                    Codec.FLOAT.fieldOf("r").forGetter(option -> option.red),
-                    Codec.FLOAT.fieldOf("g").forGetter(option -> option.green),
-                    Codec.FLOAT.fieldOf("b").forGetter(option -> option.blue),
+                    Codec.INT.fieldOf("color").forGetter(option -> option.color),
                     Codec.INT.fieldOf("life").forGetter(option -> option.life),
                     Codec.FLOAT.fieldOf("size").forGetter(option -> option.size),
                     Codec.BOOL.fieldOf("cooldown").forGetter(option -> option.cooldown),
@@ -27,7 +23,7 @@ public class CustomCloudOption implements ParticleOptions {
             ).apply(builder, CustomCloudOption::new));
 
     public static final StreamCodec<ByteBuf, CustomCloudOption> STREAM_CODEC = StreamCodec.composite(
-            ByteBufCodecs.VECTOR3F, CustomCloudOption::getColor,
+            ByteBufCodecs.INT, CustomCloudOption::getColor,
             ByteBufCodecs.INT, CustomCloudOption::getLife,
             ByteBufCodecs.FLOAT, CustomCloudOption::getSize,
             ByteBufCodecs.BOOL, CustomCloudOption::getCooldown,
@@ -35,43 +31,39 @@ public class CustomCloudOption implements ParticleOptions {
             CustomCloudOption::new
     );
 
-    private final float red;
-    private final float green;
-    private final float blue;
+    private final int color;
     private final int life;
     private final float size;
     private final boolean cooldown;
     private final boolean light;
 
 
-    public CustomCloudOption(Vector3f color, int life, float size, boolean cooldown, boolean light) {
-        this(color.x, color.y, color.z, life, size, cooldown, light);
+    public CustomCloudOption(float r, float g, float b, int life, float size, boolean cooldown, boolean light) {
+        this(Math.round(r * 255) << 16 | Math.round(g * 255) << 8 | Math.round(b * 255), life, size, cooldown, light);
     }
 
-    public CustomCloudOption(float r, float g, float b, int life, float size, boolean cooldown, boolean light) {
-        this.red = r;
-        this.green = g;
-        this.blue = b;
+    public CustomCloudOption(int color, int life, float size, boolean cooldown, boolean light) {
+        this.color = color;
         this.life = life;
         this.size = size;
         this.cooldown = cooldown;
         this.light = light;
     }
 
-    public Vector3f getColor() {
-        return new Vector3f(red, green, blue);
+    public int getColor() {
+        return color;
     }
 
     public float getRed() {
-        return red;
+        return (this.color >> 16 & 255) / 255f;
     }
 
     public float getGreen() {
-        return green;
+        return (this.color >> 8 & 255) / 255f;
     }
 
     public float getBlue() {
-        return blue;
+        return (this.color & 255) / 255f;
     }
 
     public int getLife() {
