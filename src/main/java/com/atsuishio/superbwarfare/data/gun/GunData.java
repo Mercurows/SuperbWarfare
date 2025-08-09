cb97be5f55b82c17332de1b1c00417bc254b6a9b
Light_Quanta <zch22248@foxmail.com>
2025-08-09 21:59:26 +0800

    修改PropertyModifier应用顺序

--- src/main/java/com/atsuishio/superbwarfare/data/gun/GunData.java
+++ src/main/java/com/atsuishio/superbwarfare/data/gun/GunData.java
@@ -225,9 +225,6 @@ public class GunData {
         // gun modifiers
         modifier.apply(this.item.getModifier(prop));
 
-        // AmmoConsumer
-        modifier.apply(selectedAmmoConsumer().getModifier(prop));
-
         // property override tag
         if (!propertyOverrideString.get().isEmpty()) {
             if (!propertyOverrideCache.getFirst().equals(propertyOverrideString.get())) {
@@ -243,7 +240,6 @@ public class GunData {
             var propJson = propertyOverrideCache.getSecond();
             if (propJson != null && propJson.has(prop.name) && isOverrideValid) {
                 try {
-                    // TODO 无法在这里应用AmmoConsumer的modifier，是否考虑支持？
                     var parsedValue = DataLoader.processValue(GSON.fromJson(propJson.get(prop.name).toString(), prop.getFieldType()));
                     modifier.apply((data, value) -> (T) parsedValue);
                 } catch (Exception exception) {
@@ -252,6 +248,9 @@ public class GunData {
                 }
             }
         }
+
+        // AmmoConsumer
+        modifier.apply(selectedAmmoConsumer().getModifier(prop));
 
         // perk
         if (perk != null) {
