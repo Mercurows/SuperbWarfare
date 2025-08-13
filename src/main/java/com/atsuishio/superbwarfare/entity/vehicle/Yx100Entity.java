249a8358c77f79ab264a5fb562a48b56436f4b7a
Atsuishio <842960157@qq.com>
2025-08-14 03:15:24 +0800

    玩家背包内有创造弹药盒时退弹不返还弹药

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Yx100Entity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Yx100Entity.java
@@ -1046,13 +1046,21 @@ public class Yx100Entity extends ContainerMobileVehicleEntity implements GeoEnti
         var typeIndex = isScroll ? (value + getWeaponIndex(index) + count) % count : value;
 
         if (typeIndex == 0 || typeIndex == 1 || typeIndex == 2) {
+            boolean hasCreativeAmmo = false;
+            for (int i = 0; i < getMaxPassengers(); i++) {
+                if (getNthEntity(i) instanceof Player pPlayer && InventoryTool.hasCreativeAmmoBox(pPlayer)) {
+                    hasCreativeAmmo = true;
+                }
+            }
+
+
             if (typeIndex != entityData.get(SELECTED_AMMO_TYPE)) {
                 this.reloadCoolDown = 80;
                 Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(entityData.get(LOADED_SHELL)));
                 if (item == null) {
                     return;
                 }
-                if (!this.entityData.get(LOADED_SHELL).equals("null")) {
+                if (!this.entityData.get(LOADED_SHELL).equals("null") && !hasCreativeAmmo) {
                     this.insertItem(new ItemStack(item).getItem(), 1);
                 }
                 entityData.set(LOADED_SHELL, "null");
