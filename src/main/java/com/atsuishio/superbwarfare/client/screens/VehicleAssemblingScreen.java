cd29cc14388a4654c75d2c4a5cbc8de9bab70945
17146 <1714673995@qq.com>
2025-08-14 15:46:27 +0800

    添加载具装配台screen的初步渲染

--- src/main/java/com/atsuishio/superbwarfare/client/screens/VehicleAssemblingScreen.java
+++ src/main/java/com/atsuishio/superbwarfare/client/screens/VehicleAssemblingScreen.java
@@ -1,9 +1,11 @@
 package com.atsuishio.superbwarfare.client.screens;
 
+import com.atsuishio.superbwarfare.Mod;
 import com.atsuishio.superbwarfare.menu.VehicleAssemblingMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
@@ -12,8 +14,13 @@ import org.jetbrains.annotations.NotNull;
 @OnlyIn(Dist.CLIENT)
 public class VehicleAssemblingScreen extends AbstractContainerScreen<VehicleAssemblingMenu> {
 
+    private static final ResourceLocation TEXTURE = Mod.loc("textures/gui/vehicle_assembling_table.png");
+    private static final int IMAGE_SIZE = 324;
+
     public VehicleAssemblingScreen(VehicleAssemblingMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
+        imageWidth = 322;
+        imageHeight = 178;
     }
 
     @Override
@@ -25,6 +32,14 @@ public class VehicleAssemblingScreen extends AbstractContainerScreen<VehicleAsse
 
     @Override
     protected void renderBg(@NotNull GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+        int i = (this.width - this.imageWidth) / 2;
+        int j = (this.height - this.imageHeight) / 2;
+        pGuiGraphics.blit(TEXTURE, i, j, 0, 0, this.imageWidth, this.imageHeight, IMAGE_SIZE, IMAGE_SIZE);
 
+    }
+
+    // 本方法留空
+    @Override
+    protected void renderLabels(@NotNull GuiGraphics pGuiGraphics, int pMouseX, int pMouseY) {
     }
 }
