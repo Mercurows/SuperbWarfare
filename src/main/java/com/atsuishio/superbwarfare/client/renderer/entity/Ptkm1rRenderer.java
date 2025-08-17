9c1a5708f7476ea3600a0a2100e42b152871ab97
17146 <1714673995@qq.com>
2025-08-17 15:36:19 +0800

    修复ptkm渲染问题

--- src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/Ptkm1rRenderer.java
+++ src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/Ptkm1rRenderer.java
@@ -9,7 +9,6 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -25,20 +24,6 @@ public class Ptkm1rRenderer extends GeoEntityRenderer<Ptkm1rEntity> {
     }
 
     @Override
-    public void preRender(PoseStack poseStack, Ptkm1rEntity entity, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green,
-                          float blue, float alpha) {
-        float scale = 1f;
-        this.scaleHeight = scale;
-        this.scaleWidth = scale;
-        super.preRender(poseStack, entity, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
-    }
-
-    @Override
-    public void render(Ptkm1rEntity entityIn, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource bufferIn, int packedLightIn) {
-        super.render(entityIn, entityYaw, partialTicks, poseStack, bufferIn, packedLightIn);
-    }
-
-    @Override
     public void renderRecursively(PoseStack poseStack, Ptkm1rEntity entityIn, GeoBone bone, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         String name = bone.getName();
         if (name.equals("body")) {
@@ -47,5 +32,6 @@ public class Ptkm1rRenderer extends GeoEntityRenderer<Ptkm1rEntity> {
         if (name.equals("zhu2")) {
             bone.setRotX(0.5f * Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot()) * Mth.DEG_TO_RAD);
         }
+        super.renderRecursively(poseStack, entityIn, bone, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
