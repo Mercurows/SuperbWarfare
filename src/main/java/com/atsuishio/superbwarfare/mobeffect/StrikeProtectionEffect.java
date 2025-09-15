516439ba669d6664bf075289cdd17ed3ac7bda31
17146 <1714673995@qq.com>
2025-09-15 21:23:01 +0800

    移除冗余代码

--- src/main/java/com/atsuishio/superbwarfare/mobeffect/StrikeProtectionEffect.java
+++ src/main/java/com/atsuishio/superbwarfare/mobeffect/StrikeProtectionEffect.java
@@ -4,6 +4,7 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 
 public class StrikeProtectionEffect extends MobEffect {
+
     public StrikeProtectionEffect() {
         super(MobEffectCategory.BENEFICIAL, -12708330);
     }
