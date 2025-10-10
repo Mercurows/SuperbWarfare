51b13feecb7dbaf20c6f40de5c79ced94e0a40a7
17146 <1714673995@qq.com>
2025-10-11 06:56:00 +0800

    添加部分载具物品栏的渲染

--- src/main/java/com/atsuishio/superbwarfare/client/screens/VehicleScreen.java
+++ src/main/java/com/atsuishio/superbwarfare/client/screens/VehicleScreen.java
@@ -1,6 +1,7 @@
 package com.atsuishio.superbwarfare.client.screens;
 
 import com.atsuishio.superbwarfare.Mod;
+import com.atsuishio.superbwarfare.data.vehicle.subdata.VehicleContainerType;
 import com.atsuishio.superbwarfare.menu.VehicleMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -11,22 +12,68 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
 
+// TODO 完成渲染
 @OnlyIn(Dist.CLIENT)
 public class VehicleScreen extends AbstractContainerScreen<VehicleMenu> {
 
-    private static final ResourceLocation TEXTURE = Mod.loc("textures/gui/vehicle.png");
+    private static final ResourceLocation MINI = Mod.loc("textures/gui/vehicle/inventory/mini.png");
+    private static final ResourceLocation SMALL = Mod.loc("textures/gui/vehicle/inventory/small.png");
+    private static final ResourceLocation MEDIUM = Mod.loc("textures/gui/vehicle/inventory/medium.png");
+    private static final ResourceLocation LARGE = Mod.loc("textures/gui/vehicle/inventory/large.png");
+    private static final ResourceLocation HUGE = Mod.loc("textures/gui/vehicle/inventory/huge.png");
 
-    public VehicleScreen(VehicleMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+    private static final ResourceLocation INVENTORY = Mod.loc("textures/gui/vehicle/inventory/player_inventory.png");
+    private static final ResourceLocation INVENTORY_UPGRADE = Mod.loc("textures/gui/vehicle/inventory/player_inventory_upgrade.png");
+
+    private final VehicleContainerType type;
+    private final boolean hasUpgradeSlots;
+
+    public static final int X_OFFSET = 8;
+    public static final int INVENTORY_TEXTURE_SIZE = 256;
+
+    public VehicleScreen(VehicleMenu pMenu, Inventory pPlayerInventory, Component pTitle, VehicleContainerType type) {
         super(pMenu, pPlayerInventory, pTitle);
-        imageWidth = 345;
-        imageHeight = 222;
+        this.type = type;
+        this.hasUpgradeSlots = pMenu.hasUpgradeSlots();
+
+        this.imageWidth = switch (type) {
+            default -> 222;
+            case HUGE -> 320;
+        };
+        this.imageHeight = switch (type) {
+            default -> 132;
+            case HUGE -> 132;
+        } + 90;
     }
 
     @Override
     protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
         int i = (this.width - this.imageWidth) / 2;
         int j = (this.height - this.imageHeight) / 2;
-        pGuiGraphics.blit(TEXTURE, i, j, 0, 0, this.imageWidth, this.imageHeight, 358, 328);
+
+        var texture = switch (type) {
+            default -> null;
+            case MINI -> MINI;
+            case SMALL -> SMALL;
+            case MEDIUM -> MEDIUM;
+            case LARGE -> LARGE;
+            case HUGE -> HUGE;
+        };
+        if (texture == null) return;
+
+        int size = this.type == VehicleContainerType.HUGE ? 328 : 256;
+        pGuiGraphics.blit(texture, i + X_OFFSET, j, 0, 0, this.imageWidth, this.imageHeight, size, size);
+
+        if (this.hasUpgradeSlots) {
+            pGuiGraphics.blit(INVENTORY_UPGRADE, i - 24 + X_OFFSET, j + this.imageHeight - 90, 0, 0, 198, 90, INVENTORY_TEXTURE_SIZE, INVENTORY_TEXTURE_SIZE);
+        } else {
+            pGuiGraphics.blit(INVENTORY, i + X_OFFSET, j + this.imageHeight - 90, 0, 0, 175, 90, INVENTORY_TEXTURE_SIZE, INVENTORY_TEXTURE_SIZE);
+        }
+    }
+
+    @Override
+    protected void renderLabels(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY) {
+        pGuiGraphics.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 4210752, false);
     }
 
     @Override
@@ -41,7 +88,7 @@ public class VehicleScreen extends AbstractContainerScreen<VehicleMenu> {
         super.init();
         this.titleLabelX = 33;
         this.titleLabelY = 5;
-        this.inventoryLabelX = 105;
+        this.inventoryLabelX = (this.menu.getContainerCols() - 9) / 2 * 18 + this.titleLabelX;
         this.inventoryLabelY = 128;
     }
 }
--- src/main/java/com/atsuishio/superbwarfare/init/ModScreens.java
+++ src/main/java/com/atsuishio/superbwarfare/init/ModScreens.java
@@ -2,6 +2,10 @@ package com.atsuishio.superbwarfare.init;
 
 import com.atsuishio.superbwarfare.Mod;
 import com.atsuishio.superbwarfare.client.screens.*;
+import com.atsuishio.superbwarfare.data.vehicle.subdata.VehicleContainerType;
+import com.atsuishio.superbwarfare.menu.VehicleMenu;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -15,7 +19,32 @@ public class ModScreens {
     public static void clientLoad(RegisterMenuScreensEvent event) {
         event.register(ModMenuTypes.REFORGING_TABLE_MENU.get(), ReforgingTableScreen::new);
         event.register(ModMenuTypes.CHARGING_STATION_MENU.get(), ChargingStationScreen::new);
-        event.register(ModMenuTypes.VEHICLE_MENU_HUGE.get(), VehicleScreen::new);
+
+        event.register(ModMenuTypes.VEHICLE_MENU_MINI.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.MINI));
+        event.register(ModMenuTypes.VEHICLE_MENU_MINI_UPGRADE.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.MINI));
+
+        event.register(ModMenuTypes.VEHICLE_MENU_SMALL.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.SMALL));
+        event.register(ModMenuTypes.VEHICLE_MENU_SMALL_UPGRADE.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.SMALL));
+
+        event.register(ModMenuTypes.VEHICLE_MENU_MEDIUM.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.MEDIUM));
+        event.register(ModMenuTypes.VEHICLE_MENU_MEDIUM_UPGRADE.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.MEDIUM));
+
+        event.register(ModMenuTypes.VEHICLE_MENU_LARGE.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.LARGE));
+        event.register(ModMenuTypes.VEHICLE_MENU_LARGE_UPGRADE.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.LARGE));
+
+        event.register(ModMenuTypes.VEHICLE_MENU_HUGE.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.HUGE));
+        event.register(ModMenuTypes.VEHICLE_MENU_HUGE_UPGRADE.get(),
+                (VehicleMenu menu, Inventory inventory, Component title) -> new VehicleScreen(menu, inventory, title, VehicleContainerType.HUGE));
+
         event.register(ModMenuTypes.SUPERB_ITEM_INTERFACE_MENU.get(), SuperbItemInterfaceScreen::new);
         event.register(ModMenuTypes.FUMO_25_MENU.get(), FuMO25Screen::new);
         event.register(ModMenuTypes.VEHICLE_ASSEMBLING_MENU.get(), VehicleAssemblingScreen::new);
