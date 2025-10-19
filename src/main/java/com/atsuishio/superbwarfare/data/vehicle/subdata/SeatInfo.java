56b4f17460e2da51c00e16a0fb3acd67200d8a57
Light_Quanta <zch22248@foxmail.com>
2025-10-19 15:26:23 +0800

    添加座位锚点相关属性

--- src/main/java/com/atsuishio/superbwarfare/data/vehicle/subdata/SeatInfo.java
+++ src/main/java/com/atsuishio/superbwarfare/data/vehicle/subdata/SeatInfo.java
@@ -1,8 +1,10 @@
 package com.atsuishio.superbwarfare.data.vehicle.subdata;
 
 import com.atsuishio.superbwarfare.annotation.ServerOnly;
+import com.atsuishio.superbwarfare.data.StringOrVec3;
 import com.atsuishio.superbwarfare.data.gun.DefaultGunData;
 import com.google.gson.annotations.SerializedName;
+import net.minecraft.world.phys.Vec3;
 
 public class SeatInfo {
     @SerializedName("HidePassenger")
@@ -12,8 +14,28 @@ public class SeatInfo {
     @ServerOnly
     public Boolean isEnclosed = null;
 
+    // TODO 座位锚点
+    @SerializedName("Transform")
+    public String transform = "Default";
+
+    @SerializedName("Position")
+    public Vec3 position = Vec3.ZERO;
+
+    @SerializedName("Orientation")
+    public StringOrVec3 orientation = new StringOrVec3("Default");
+
+    @SerializedName("MinPitch")
+    public float minPitch = -20;
+
+    @SerializedName("MaxPitch")
+    public float maxPitch = 20;
+
+    @SerializedName("MinYaw")
+    public float minYaw = -90;
+
+    @SerializedName("MaxYaw")
+    public float maxYaw = 90;
+
     @SerializedName("WeaponData")
     public DefaultGunData weaponData = null;
-
-    // TODO 座位锚点
 }
