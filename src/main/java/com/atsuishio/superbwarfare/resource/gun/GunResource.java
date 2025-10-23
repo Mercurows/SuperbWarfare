5648398b96335c1f708ec67ea0f1e50885e693b4
17146 <1714673995@qq.com>
2025-10-23 23:04:44 +0800

    添加gun resource缓存和部分方法

--- src/main/java/com/atsuishio/superbwarfare/resource/gun/GunResource.java
+++ src/main/java/com/atsuishio/superbwarfare/resource/gun/GunResource.java
@@ -1,21 +1,31 @@
 package com.atsuishio.superbwarfare.resource.gun;
 
+import com.atsuishio.superbwarfare.data.CustomData;
 import com.atsuishio.superbwarfare.data.DefaultDataSupplier;
 import com.atsuishio.superbwarfare.item.gun.GunItem;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Supplier;
-
 public class GunResource implements DefaultDataSupplier<DefaultGunResource> {
+
+    public static final LoadingCache<ItemStack, GunResource> RESOURCE_CACHE = CacheBuilder.newBuilder()
+            .weakKeys()
+            .build(new CacheLoader<>() {
+                public @NotNull GunResource load(@NotNull ItemStack stack) {
+                    return new GunResource(stack);
+                }
+            });
 
     public final ItemStack stack;
     public final GunItem item;
     public final String id;
 
     @NotNull
-    public Supplier<DefaultGunResource> defaultResourceSupplier;
+    public DefaultGunResource defaultResource;
 
     private GunResource(ItemStack stack) {
         if (!(stack.getItem() instanceof GunItem gunItem)) {
@@ -25,11 +35,21 @@ public class GunResource implements DefaultDataSupplier<DefaultGunResource> {
         this.item = gunItem;
         this.stack = stack;
         this.id = getRegistryId(stack.getItem());
+
+        this.defaultResource = CustomData.GUN_RESOURCE.get(id);
     }
 
     @Override
     public DefaultGunResource getDefault() {
-        return this.defaultResourceSupplier.get();
+        return this.defaultResource;
+    }
+
+    public static GunResource create(Item item) {
+        return from(new ItemStack(item));
+    }
+
+    public static GunResource from(ItemStack stack) {
+        return RESOURCE_CACHE.getUnchecked(stack);
     }
 
     public static String getRegistryId(Item item) {
