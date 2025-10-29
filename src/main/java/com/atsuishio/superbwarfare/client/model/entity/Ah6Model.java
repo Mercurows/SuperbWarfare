4ad9165a310712a391809c085544bca0ca966937
Atsuishio <842960157@qq.com>
2025-10-30 03:34:05 +0800

    修复快艇，小鸟模型

--- src/main/java/com/atsuishio/superbwarfare/client/model/entity/Ah6Model.java
+++ src/main/java/com/atsuishio/superbwarfare/client/model/entity/Ah6Model.java
@@ -1,6 +1,28 @@
 package com.atsuishio.superbwarfare.client.model.entity;
 
 import com.atsuishio.superbwarfare.entity.vehicle.Ah6Entity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
 
 public class Ah6Model extends VehicleModel<Ah6Entity> {
+
+    @Override
+    public void setCustomAnimations(Ah6Entity vehicle, long instanceId, AnimationState<Ah6Entity> animationState) {
+        super.setCustomAnimations(vehicle, instanceId, animationState);
+        float partialTick = Minecraft.getInstance().getPartialTick();
+
+        CoreGeoBone propeller = getAnimationProcessor().getBone("propeller");
+
+        if (propeller != null) {
+            propeller.setRotY(Mth.lerp(partialTick, vehicle.propellerRotO, vehicle.getPropellerRot()));
+        }
+
+        CoreGeoBone tailPropeller = getAnimationProcessor().getBone("tailPropeller");
+
+        if (tailPropeller != null) {
+            tailPropeller.setRotX(-6 * Mth.lerp(partialTick, vehicle.propellerRotO, vehicle.getPropellerRot()));
+        }
+    }
 }
--- src/main/java/com/atsuishio/superbwarfare/client/model/entity/SpeedboatModel.java
+++ src/main/java/com/atsuishio/superbwarfare/client/model/entity/SpeedboatModel.java
@@ -1,6 +1,28 @@
 package com.atsuishio.superbwarfare.client.model.entity;
 
 import com.atsuishio.superbwarfare.entity.vehicle.SpeedboatEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
 
 public class SpeedboatModel extends VehicleModel<SpeedboatEntity> {
+
+    @Override
+    public void setCustomAnimations(SpeedboatEntity vehicle, long instanceId, AnimationState<SpeedboatEntity> animationState) {
+        super.setCustomAnimations(vehicle, instanceId, animationState);
+        float partialTick = Minecraft.getInstance().getPartialTick();
+
+        CoreGeoBone propeller = getAnimationProcessor().getBone("propeller");
+
+        if (propeller != null) {
+            propeller.setRotZ(Mth.lerp(partialTick, vehicle.propellerRotO, vehicle.getPropellerRot()));
+        }
+
+        CoreGeoBone rudder = getAnimationProcessor().getBone("rudder");
+
+        if (rudder != null) {
+            rudder.setRotY(Mth.lerp(partialTick, vehicle.rudderRotO, vehicle.getRudderRot()));
+        }
+    }
 }
