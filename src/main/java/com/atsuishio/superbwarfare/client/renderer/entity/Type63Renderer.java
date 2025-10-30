dc1e12b0c322acdc0689d2b75823162b2cf5cf30
Atsuishio <842960157@qq.com>
2025-10-30 16:10:04 +0800

    修复63式渲染

--- src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/Type63Renderer.java
+++ src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/Type63Renderer.java
@@ -3,16 +3,9 @@ package com.atsuishio.superbwarfare.client.renderer.entity;
 import com.atsuishio.superbwarfare.client.model.entity.Type63Model;
 import com.atsuishio.superbwarfare.entity.vehicle.Type63Entity;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.util.Mth;
-import org.jetbrains.annotations.NotNull;
-import software.bernie.geckolib.cache.object.GeoBone;
-
-import static com.atsuishio.superbwarfare.entity.vehicle.Type63Entity.LOADED_AMMO;
+import net.minecraft.world.phys.Vec3;
 
 
 public class Type63Renderer extends VehicleRenderer<Type63Entity> {
@@ -23,46 +16,8 @@ public class Type63Renderer extends VehicleRenderer<Type63Entity> {
     }
 
     @Override
-    public void render(Type63Entity entityIn, float entityYaw, float partialTicks, PoseStack poseStack, @NotNull MultiBufferSource bufferIn, int packedLightIn) {
-        poseStack.pushPose();
-        poseStack.mulPose(Axis.YP.rotationDegrees(-Mth.lerp(partialTicks, entityIn.yRotO, entityIn.getYRot())));
-        super.render(entityIn, entityYaw, partialTicks, poseStack, bufferIn, packedLightIn);
-        poseStack.popPose();
-    }
-
-    @Override
-    public void renderRecursively(PoseStack poseStack, Type63Entity animatable, GeoBone bone, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int color) {
-        String name = bone.getName();
-
-        if (name.equals("wheel1")) {
-            bone.setRotX(Mth.lerp(partialTick, animatable.leftWheelRotO, animatable.getLeftWheelRot()));
-        }
-        if (name.equals("wheel2")) {
-            bone.setRotX(Mth.lerp(partialTick, animatable.rightWheelRotO, animatable.getRightWheelRot()));
-        }
-
-        if (name.equals("main")) {
-            bone.setRotY(turretYRot * Mth.DEG_TO_RAD);
-        }
-
-        if (name.equals("paotou")) {
-            bone.setRotX(-turretXRot * Mth.DEG_TO_RAD);
-        }
-
-        if (name.equals("shoulunx")) {
-            bone.setRotX(-turretXRot * 3);
-        }
-
-        if (name.equals("shouluny")) {
-            bone.setRotZ(-turretYRot * 6);
-        }
-
-        if (name.startsWith("shell") && name.length() > 5) {
-            var items = animatable.getEntityData().get(LOADED_AMMO);
-            int i = Integer.parseInt(name.substring(5));
-            bone.setHidden(items.get(i) == -1);
-        }
-
-        super.renderRecursively(poseStack, animatable, bone, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, color);
+    public void vehicleAxis(Type63Entity entityIn, PoseStack poseStack, float entityYaw, float partialTicks) {
+        Vec3 root = new Vec3(0, entityIn.rotateYOffset(), 0);
+        poseStack.rotateAround(Axis.YP.rotationDegrees(-entityYaw), (float) root.x, (float) root.y, (float) root.z);
     }
 }
