6f57910de8f12f089c595c728b580efb16090c84
Atsuishio <842960157@qq.com>
2025-10-30 21:26:03 +0800

    修复部分载具旋转轴

--- src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/AnnihilatorRenderer.java
+++ src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/AnnihilatorRenderer.java
@@ -3,7 +3,10 @@ package com.atsuishio.superbwarfare.client.renderer.entity;
 import com.atsuishio.superbwarfare.client.layer.vehicle.*;
 import com.atsuishio.superbwarfare.client.model.entity.AnnihilatorModel;
 import com.atsuishio.superbwarfare.entity.vehicle.AnnihilatorEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.phys.Vec3;
 
 public class AnnihilatorRenderer extends VehicleRenderer<AnnihilatorEntity> {
 
@@ -15,5 +18,11 @@ public class AnnihilatorRenderer extends VehicleRenderer<AnnihilatorEntity> {
         this.addRenderLayer(new AnnihilatorPowerLightLayer(this));
         this.addRenderLayer(new AnnihilatorLedLayer(this));
         this.addRenderLayer(new AnnihilatorLedLightLayer(this));
+    }
+
+    @Override
+    public void vehicleAxis(AnnihilatorEntity entityIn, PoseStack poseStack, float entityYaw, float partialTicks) {
+        Vec3 root = new Vec3(0, entityIn.rotateYOffset(), 0);
+        poseStack.rotateAround(Axis.YP.rotationDegrees(-entityYaw), (float) root.x, (float) root.y, (float) root.z);
     }
 }
--- src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/MortarRenderer.java
+++ src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/MortarRenderer.java
@@ -4,15 +4,10 @@ import com.atsuishio.superbwarfare.client.model.entity.MortarModel;
 import com.atsuishio.superbwarfare.entity.vehicle.MortarEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
-import org.jetbrains.annotations.NotNull;
-import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import net.minecraft.world.phys.Vec3;
 
-public class MortarRenderer extends GeoEntityRenderer<MortarEntity> {
+public class MortarRenderer extends VehicleRenderer<MortarEntity> {
 
     public MortarRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new MortarModel());
@@ -20,20 +15,8 @@ public class MortarRenderer extends GeoEntityRenderer<MortarEntity> {
     }
 
     @Override
-    public RenderType getRenderType(MortarEntity animatable, ResourceLocation texture, MultiBufferSource bufferSource, float partialTick) {
-        return RenderType.entityTranslucent(getTextureLocation(animatable));
-    }
-
-    @Override
-    public void render(MortarEntity entityIn, float entityYaw, float partialTicks, PoseStack poseStack, @NotNull MultiBufferSource bufferIn, int packedLightIn) {
-        poseStack.pushPose();
-        poseStack.mulPose(Axis.YP.rotationDegrees(-Mth.lerp(partialTicks, entityIn.yRotO, entityIn.getYRot())));
-        super.render(entityIn, entityYaw, partialTicks, poseStack, bufferIn, packedLightIn);
-        poseStack.popPose();
-    }
-
-    @Override
-    public boolean shouldShowName(MortarEntity animatable) {
-        return animatable.hasCustomName();
+    public void vehicleAxis(MortarEntity entityIn, PoseStack poseStack, float entityYaw, float partialTicks) {
+        Vec3 root = new Vec3(0, entityIn.rotateYOffset(), 0);
+        poseStack.rotateAround(Axis.YP.rotationDegrees(-entityYaw), (float) root.x, (float) root.y, (float) root.z);
     }
 }
