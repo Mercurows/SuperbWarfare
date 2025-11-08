556184cb3c93b504dd5afe22740af392ecb4abf0
Light_Quanta <zch22248@foxmail.com>
2025-11-08 16:22:12 +0800

    优化Prop的移除注解

--- src/main/java/com/atsuishio/superbwarfare/data/PropModifier.java
+++ src/main/java/com/atsuishio/superbwarfare/data/PropModifier.java
@@ -3,14 +3,15 @@ package com.atsuishio.superbwarfare.data;
 import java.util.HashMap;
 import java.util.Map;
 
-@Deprecated
-public class DeprecatedPropModifier<DATA extends DefaultDataSupplier<DEFAULT_DATA>, DEFAULT_DATA, FIELD> {
+@Deprecated(forRemoval = true)
+@SuppressWarnings("removal")
+public class PropModifier<DATA extends DefaultDataSupplier<DEFAULT_DATA>, DEFAULT_DATA, FIELD> {
     private final Prop<DATA, DEFAULT_DATA, FIELD> prop;
     private final DATA data;
 
     private final Map<Prop<DATA, DEFAULT_DATA, ?>, Object> props = new HashMap<>();
 
-    DeprecatedPropModifier(Prop<DATA, DEFAULT_DATA, FIELD> prop, DATA data) {
+    PropModifier(Prop<DATA, DEFAULT_DATA, FIELD> prop, DATA data) {
         this.prop = prop;
         this.data = data;
     }
