52af2ae252ba5f9a790e0977de8fd5d6ecb65d1a
Atsuishio <842960157@qq.com>
2025-11-20 23:59:22 +0800

    莫名其妙修复了弹药过穿问题？

--- src/main/java/com/atsuishio/superbwarfare/mixins/ProjectileUtilMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/ProjectileUtilMixin.java
@@ -32,7 +32,7 @@ public class ProjectileUtilMixin {
     @Inject(method = "getEntityHitResult(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;F)Lnet/minecraft/world/phys/EntityHitResult;",
             at = @At("HEAD"), cancellable = true)
     private static void getEntityHitResult(Level pLevel, Entity pProjectile, Vec3 pStartVec, Vec3 pEndVec, AABB pBoundingBox, Predicate<Entity> pFilter, float pInflationAmount, CallbackInfoReturnable<EntityHitResult> cir) {
-        for (var entity : pLevel.getEntities(pProjectile, pBoundingBox.inflate(5), pFilter)) {
+        for (var entity : pLevel.getEntities(pProjectile, pBoundingBox, pFilter)) {
             if (entity instanceof OBBEntity obbEntity) {
                 if (pProjectile instanceof Projectile projectile &&
                         (projectile.getOwner() == entity || entity.getPassengers().contains(projectile.getOwner()))) {
@@ -40,14 +40,14 @@ public class ProjectileUtilMixin {
                 }
                 var obbList = obbEntity.getOBBs();
                 for (var obb : obbList) {
+                    obb = obb.inflate(entity.getPickRadius() * 2);
                     Optional<Vector3f> optional = obb.clip(pStartVec.toVector3f(), pEndVec.toVector3f());
-                    if (optional.isPresent()) {
-                        double d1 = pStartVec.distanceToSqr(new Vec3(optional.get()));
-                        if (d1 < Double.MAX_VALUE) {
-                            EntityHitResult hitResult = new EntityHitResult(entity, new Vec3(optional.get()));
+                    double pDistance = pStartVec.distanceToSqr(pEndVec);
+                    if (obb.contains(pStartVec)) {
+                        if (pDistance >= 0) {
+                            EntityHitResult hitResult = new EntityHitResult(entity, new Vec3(optional.orElse(pStartVec.toVector3f())));
                             var acc = OBBHitter.getInstance(pProjectile);
                             acc.sbw$setCurrentHitPart(obb.part());
-
                             cir.setReturnValue(hitResult);
                             if (pLevel instanceof ServerLevel serverLevel && pProjectile.getDeltaMovement().lengthSqr() > 0.01 && pProjectile instanceof Projectile) {
                                 Vec3 hitPos = hitResult.getLocation();
@@ -55,6 +55,23 @@ public class ProjectileUtilMixin {
                                 sendParticle(serverLevel, ModParticleTypes.FIRE_STAR.get(), hitPos.x, hitPos.y, hitPos.z, 2, 0, 0, 0, 0.2, false);
                                 sendParticle(serverLevel, ParticleTypes.SMOKE, hitPos.x, hitPos.y, hitPos.z, 2, 0, 0, 0, 0.01, false);
                             }
+                            return;
+                        }
+                    } else if (optional.isPresent()) {
+                        var vec = new Vec3(optional.get());
+                        double d1 = pStartVec.distanceToSqr(vec);
+                        if (d1 < pDistance || pDistance == 0) {
+                            EntityHitResult hitResult = new EntityHitResult(entity, vec);
+                            var acc = OBBHitter.getInstance(pProjectile);
+                            acc.sbw$setCurrentHitPart(obb.part());
+                            cir.setReturnValue(hitResult);
+                            if (pLevel instanceof ServerLevel serverLevel && pProjectile.getDeltaMovement().lengthSqr() > 0.01 && pProjectile instanceof Projectile) {
+                                Vec3 hitPos = hitResult.getLocation();
+                                pLevel.playSound(null, BlockPos.containing(hitPos), ModSounds.HIT.get(), SoundSource.PLAYERS, 1, 1);
+                                sendParticle(serverLevel, ModParticleTypes.FIRE_STAR.get(), hitPos.x, hitPos.y, hitPos.z, 2, 0, 0, 0, 0.2, false);
+                                sendParticle(serverLevel, ParticleTypes.SMOKE, hitPos.x, hitPos.y, hitPos.z, 2, 0, 0, 0, 0.01, false);
+                            }
+                            return;
                         }
                     }
                 }
@@ -62,7 +79,6 @@ public class ProjectileUtilMixin {
         }
     }
 
-    // TODO 这个似乎有问题，OBB背后还有OBB时会返回后面那个
     @Inject(method = "getEntityHitResult(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;D)Lnet/minecraft/world/phys/EntityHitResult;",
             at = @At("HEAD"), cancellable = true)
     private static void getEntityHitResult(Entity pShooter, Vec3 pStartVec, Vec3 pEndVec, AABB pBoundingBox, Predicate<Entity> pFilter, double pDistance, CallbackInfoReturnable<EntityHitResult> cir) {
