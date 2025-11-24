3901f94126f6ead69e58cc609eacd97b24804978
Atsuishio <842960157@qq.com>
2025-11-24 21:38:14 +0800

    修复汤姆渲染

--- src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/Tom6Renderer.java
+++ src/main/java/com/atsuishio/superbwarfare/client/renderer/entity/Tom6Renderer.java
@@ -3,9 +3,8 @@ package com.atsuishio.superbwarfare.client.renderer.entity;
 import com.atsuishio.superbwarfare.client.model.entity.Tom6Model;
 import com.atsuishio.superbwarfare.entity.vehicle.Tom6Entity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-public class Tom6Renderer extends GeoEntityRenderer<Tom6Entity> {
+public class Tom6Renderer extends VehicleRenderer<Tom6Entity> {
 
     public Tom6Renderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new Tom6Model());
