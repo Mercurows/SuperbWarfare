04a73e2eeade937e38022e9fc3f85f0d196c0f52
Light_Quanta <zch22248@foxmail.com>
2025-11-28 18:01:06 +0800

    优化小鸟damageModifier

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Ah6Entity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Ah6Entity.java
@@ -2,7 +2,6 @@ package com.atsuishio.superbwarfare.entity.vehicle;
 
 import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity;
 import com.atsuishio.superbwarfare.entity.vehicle.damage.DamageModifier;
-import com.atsuishio.superbwarfare.init.ModTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 
@@ -15,14 +14,7 @@ public class Ah6Entity extends GeoVehicleEntity {
     @Override
     public DamageModifier getDamageModifier() {
         return super.getDamageModifier()
-                .custom((source, damage) -> {
-                    var entity = source.getDirectEntity();
-                    if (entity != null && entity.getType().is(ModTags.EntityTypes.AERIAL_BOMB)) {
-                        damage *= 2;
-                    }
-                    damage *= getHealth() > 0.1f ? 0.7f : 0.05f;
-                    return damage;
-                });
+                .custom((source, damage) -> damage * getHealth() > 0.1f ? 0.7f : 0.05f);
     }
 
     @Override
