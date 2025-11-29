8e6fa150525ef36f16f6f98f03cadc0c783ec001
17146 <1714673995@qq.com>
2025-11-29 15:06:57 +0800

    调整HUD数据命名

--- src/main/java/com/atsuishio/superbwarfare/client/overlay/VehicleMainWeaponHudOverlay.java
+++ src/main/java/com/atsuishio/superbwarfare/client/overlay/VehicleMainWeaponHudOverlay.java
@@ -66,7 +66,7 @@ public class VehicleMainWeaponHudOverlay implements IGuiOverlay {
         if (!(player.getVehicle() instanceof VehicleEntity vehicle)) return;
         if (ClientEventHandler.isEditing) return;
 
-        var type = vehicle.computed().turretHudType;
+        var type = vehicle.computed().hudType;
         if (type.equals(EMPTY)) return;
 
         var gunData = vehicle.getGunData(player);
