6e7b51b3434a73bbec3a156280827d0fea82fae4
Light_Quanta <zch22248@foxmail.com>
2025-12-01 10:26:30 +0800

    kt化载具model

---
+++ src/main/java/com/atsuishio/superbwarfare/client/model/entity/TowModel.kt
@@ -0,0 +1,24 @@
+package com.atsuishio.superbwarfare.client.model.entity
+
+import com.atsuishio.superbwarfare.MC
+import com.atsuishio.superbwarfare.entity.vehicle.TowEntity
+import com.atsuishio.superbwarfare.event.ClientEventHandler
+import net.minecraft.client.CameraType
+
+class TowModel : VehicleModel<TowEntity>() {
+    override fun collectTransform(boneName: String): TransformContext<TowEntity>? {
+        return when (boneName) {
+            "guanmiao" -> TransformContext { bone, vehicle, _ ->
+                val player = MC.player
+                bone.isHidden =
+                    vehicle.getFirstPassenger() === player && (MC.options.cameraType == CameraType.FIRST_PERSON || ClientEventHandler.zoomVehicle)
+            }
+
+            "missile" -> TransformContext { bone, vehicle, _ ->
+                bone.isHidden = !vehicle.getEntityData().get(TowEntity.LOADED)
+            }
+
+            else -> super.collectTransform(boneName)
+        }
+    }
+}
