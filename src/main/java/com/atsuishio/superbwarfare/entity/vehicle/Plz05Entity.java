97588ae2db2a76c7089a1054cd93661f8c08cab1
17146 <1714673995@qq.com>
2025-12-03 19:36:35 +0800

    添加炮塔类实体能否被绑定的方法

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Plz05Entity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Plz05Entity.java
@@ -37,4 +37,9 @@ public class Plz05Entity extends ArtilleryEntity {
     public void registerControllers(AnimatableManager.ControllerRegistrar data) {
         data.add(new AnimationController<>(this, "shoot", 0, this::shootPredicate));
     }
+
+    @Override
+    public boolean canBind() {
+        return true;
+    }
 }
