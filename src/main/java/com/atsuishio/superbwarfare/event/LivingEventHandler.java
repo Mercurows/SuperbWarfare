afb02e5530dbbb888e102cbdc3fa112ec9727218
Atsuishio <842960157@qq.com>
2025-12-12 13:53:38 +0800

    修复封闭机舱飞机坠机是驾驶员无伤bug

--- src/main/java/com/atsuishio/superbwarfare/event/LivingEventHandler.java
+++ src/main/java/com/atsuishio/superbwarfare/event/LivingEventHandler.java
@@ -76,7 +76,7 @@ public class LivingEventHandler {
 
     @SubscribeEvent
     public static void onEntityAttacked(LivingIncomingDamageEvent event) {
-        if (!event.getSource().is(ModDamageTypes.VEHICLE_EXPLOSION)
+        if (!event.getSource().is(ModDamageTypes.VEHICLE_EXPLOSION) && !event.getSource().is(ModDamageTypes.AIR_CRASH)
                 && event.getEntity().getVehicle() instanceof VehicleEntity vehicle
                 && vehicle.isEnclosed(event.getEntity())
         ) {
@@ -120,7 +120,7 @@ public class LivingEventHandler {
             if (source.is(ModTags.DamageTypes.VEHICLE_IGNORE)) return;
 
             if (vehicle.isEnclosed(event.getEntity())) {
-                if (!source.is(ModDamageTypes.VEHICLE_EXPLOSION)) {
+                if (!source.is(ModDamageTypes.VEHICLE_EXPLOSION) && !source.is(ModDamageTypes.AIR_CRASH)) {
                     event.setCanceled(true);
                 }
             } else {
