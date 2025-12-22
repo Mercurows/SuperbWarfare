e9299f40ad67be85a6b199b31e87fe010f6d0622
Atsuishio <842960157@qq.com>
2025-12-22 20:08:43 +0800

    尝试mixinLevelRender里的renderEntity

--- src/main/java/com/atsuishio/superbwarfare/mixins/LevelRendererMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/LevelRendererMixin.java
@@ -2,15 +2,19 @@ package com.atsuishio.superbwarfare.mixins;
 
 import com.atsuishio.superbwarfare.tools.VectorUtil;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.world.entity.Entity;
 import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelRenderer.class)
@@ -23,5 +27,33 @@ public class LevelRendererMixin {
     private void onStartRenderLevel(DeltaTracker deltaTracker, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
         VectorUtil.modelViewMatrix = RenderSystem.getModelViewMatrix();
         VectorUtil.projectionMatrix = RenderSystem.getProjectionMatrix();
+    }
+
+    @Shadow
+    @Final
+    private EntityRenderDispatcher entityRenderDispatcher;
+
+    //TODO 正确实现mixin
+
+    @ModifyVariable(
+            method = "renderEntity",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/LevelRenderer;renderEntity(Lnet/minecraft/world/entity/Entity;DDDFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V",
+                    shift = At.Shift.AFTER
+            ),
+            index = 7, // MultiBufferSource 参数的索引
+            argsOnly = true
+    )
+    private MultiBufferSource modifyBufferSource(
+            MultiBufferSource originalBuffer,
+            Entity entity,
+            double camX, double camY, double camZ,
+            float partialTick,
+            PoseStack poseStack,
+            MultiBufferSource bufferSource // 这是原始的 bufferSource
+    ) {
+
+        return renderType -> bufferSource.getBuffer(RenderType.endPortal());
     }
 }
