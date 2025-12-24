18224824a421eeb4dc9ce156ff8c023c335b0699
Atsuishio <842960157@qq.com>
2025-12-25 01:37:56 +0800

    添加热成像夜视buff

--- src/main/java/com/atsuishio/superbwarfare/item/curio/ThermalImagingGogglesItem.java
+++ src/main/java/com/atsuishio/superbwarfare/item/curio/ThermalImagingGogglesItem.java
@@ -1,5 +1,8 @@
 package com.atsuishio.superbwarfare.item.curio;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.CuriosApi;
@@ -18,5 +21,13 @@ public class ThermalImagingGogglesItem extends Item implements ICurioItem {
                 .resolve()
                 .flatMap(c -> c.findFirstCurio(this))
                 .isEmpty();
+    }
+
+    @Override
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        LivingEntity living = slotContext.entity();
+        if (!living.level().isClientSide) {
+            living.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 300, 0, false, false));
+        }
     }
 }
