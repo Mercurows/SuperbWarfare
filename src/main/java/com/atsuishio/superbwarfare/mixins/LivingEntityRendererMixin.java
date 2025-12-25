313926b74ad41af222a487ba46fbe47e8a08a709
17146 <1714673995@qq.com>
2025-12-25 16:48:00 +0800

    修改mixin写法

--- src/main/java/com/atsuishio/superbwarfare/mixins/LivingEntityRendererMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/LivingEntityRendererMixin.java
@@ -5,21 +5,33 @@ import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity;
 import com.atsuishio.superbwarfare.entity.vehicle.utils.VehicleVecUtils;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import org.joml.Quaterniond;
 import org.joml.Quaternionf;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
-public class LivingEntityRendererMixin<T extends LivingEntity> {
+public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends EntityRenderer<T> implements RenderLayerParent<T, M> {
+
+    @Shadow
+    protected M model;
+
+    protected LivingEntityRendererMixin(EntityRendererProvider.Context pContext) {
+        super(pContext);
+    }
 
     @Inject(method = "setupRotations", at = @At("TAIL"))
     public void render(T entity, PoseStack matrices, float animationProgress, float bodyYaw, float tickDelta, CallbackInfo ci) {
@@ -48,11 +60,11 @@ public class LivingEntityRendererMixin<T extends LivingEntity> {
     }
 
     //TODO 正确实现mixin
-
     @Inject(method = "getRenderType(Lnet/minecraft/world/entity/LivingEntity;ZZZ)Lnet/minecraft/client/renderer/RenderType;",
             at = @At("HEAD"), cancellable = true)
     protected void getRenderType(T pLivingEntity, boolean pBodyVisible, boolean pTranslucent, boolean pGlowing, CallbackInfoReturnable<RenderType> cir) {
         ResourceLocation resourcelocation = this.getTextureLocation(pLivingEntity);
+
         if (pTranslucent) {
             cir.setReturnValue(RenderType.itemEntityTranslucentCull(resourcelocation));
         } else if (pBodyVisible) {
