90f7ff519b6d40fc14dcda6b7882518e346b060a
Atsuishio <842960157@qq.com>
2025-12-30 22:52:59 +0800

    调整皮卡车地形适应旋转幅度，调整机枪皮卡在创造模式选项卡的位置

--- src/main/java/com/atsuishio/superbwarfare/item/common/container/ContainerBlockItem.java
+++ src/main/java/com/atsuishio/superbwarfare/item/common/container/ContainerBlockItem.java
@@ -46,6 +46,7 @@ public class ContainerBlockItem extends BlockItem implements GeoItem {
     public static void registerContainers(RegisterContainersEvent event) {
         event.add(ModEntities.WHEEL_CHAIR);
         event.add(ModEntities.SODAYO_PICK_UP.get());
+        event.add(ModEntities.SODAYO_PICK_UP_HMG.get());
         event.add(ModEntities.TRUCK.get());
         event.add(ModEntities.TYPE_63);
         event.add(ModEntities.MK_42);
@@ -56,7 +57,6 @@ public class ContainerBlockItem extends BlockItem implements GeoItem {
         event.add(ModEntities.WAVEFORCE_TOWER);
         event.add(ModEntities.ANNIHILATOR);
         event.add(ModEntities.SPEEDBOAT);
-        event.add(ModEntities.SODAYO_PICK_UP_HMG.get());
         event.add(ModEntities.LAV_150);
         event.add(ModEntities.BMP_2);
         event.add(ModEntities.LAV_AD);
