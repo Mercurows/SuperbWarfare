4fdb4f9520e8f8dadbfa112a2c26a56101fed774
Atsuishio <842960157@qq.com>
2026-01-09 04:52:37 +0800

    优化乘客在载具上的旋转

--- src/main/java/com/atsuishio/superbwarfare/mixins/LivingEntityRendererMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/LivingEntityRendererMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import org.joml.Quaterniond;
 import org.joml.Quaternionf;
@@ -22,7 +21,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends EntityRenderer<T> implements RenderLayerParent<T, M> {
-
     @Shadow
     protected M model;
 
@@ -30,56 +28,24 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extend
         super(pContext);
     }
 
-    @Inject(method = "setupRotations", at = @At("TAIL"))
-    public void render(T entity, PoseStack poseStack, float bob, float yBodyRot, float partialTick, float scale, CallbackInfo ci) {
+    @Inject(method = "setupRotations", at = @At("HEAD"), cancellable = true)
+    public void setupRotations(T entity, PoseStack poseStack, float bob, float yBodyRot, float partialTick, float scale, CallbackInfo ci) {
         if (entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof VehicleEntity vehicle) {
             var seats = VehicleData.compute(vehicle).seats();
             int index = vehicle.getSeatIndex(entity);
             if (index < 0 || index >= seats.size()) return;
 
+            ci.cancel();
             var seat = seats.get(index);
-            float lerpRot = Mth.lerp(partialTick, entity.yBodyRotO, entity.yBodyRot);
-
-            if (!seat.canRotateBody && Mth.abs(entity.yBodyRot) > 150) {
-                lerpRot = entity.yBodyRot;
-            }
 
             float transformYaw = (float) VehicleVecUtils.getYRotFromVector(vehicle.getTransformDirectionNoOrientation(partialTick, entity));
-            var bodyDiffY = lerpRot + transformYaw;
-            var passengerWeaponStationYawRot = Axis.YP.rotationDegrees(-bodyDiffY);
+            var passengerWeaponStationYawRot = Axis.YP.rotationDegrees(-transformYaw);
 
             Quaterniond quaterniond = vehicle.getRotationFromString(seat.transform, partialTick).mul(new Quaterniond(passengerWeaponStationYawRot));
-            Quaternionf quaternionf = new Quaternionf(-quaterniond.x, quaterniond.y, -quaterniond.z, quaterniond.w);
+            Quaternionf quaternionf = new Quaternionf(quaterniond.x, quaterniond.y, quaterniond.z, quaterniond.w);
 
-            poseStack.mulPose(Axis.YP.rotationDegrees(lerpRot));
             poseStack.mulPose(quaternionf);
+            poseStack.mulPose(Axis.YP.rotationDegrees(180.0F - yBodyRot));
         }
     }
-
-//    @Inject(method = "getRenderType(Lnet/minecraft/world/entity/LivingEntity;ZZZ)Lnet/minecraft/client/renderer/RenderType;",
-//            at = @At("HEAD"), cancellable = true)
-//    protected void getRenderType(T pLivingEntity, boolean pBodyVisible, boolean pTranslucent, boolean pGlowing, CallbackInfoReturnable<RenderType> cir) {
-//        ResourceLocation resourcelocation = this.getTextureLocation(pLivingEntity);
-//
-//        if (ClientEventHandler.activeThermalImaging) {
-//            resourcelocation = getSmartBrightenedTexture(resourcelocation, 5f);
-//        }
-//
-//        if (pTranslucent) {
-//            cir.setReturnValue(RenderType.itemEntityTranslucentCull(resourcelocation));
-//        } else if (pBodyVisible) {
-//            cir.setReturnValue(this.model.renderType(resourcelocation));
-//        } else {
-//            cir.setReturnValue(pGlowing ? RenderType.outline(resourcelocation) : null);
-//        }
-//    }
-
-//    @Inject(method = "getOverlayCoords", at = @At("HEAD"), cancellable = true)
-//    private static void getOverlayCoords(LivingEntity pLivingEntity, float pU, CallbackInfoReturnable<Integer> cir) {
-////        if (ClientEventHandler.activeThermalImaging) {
-////
-////        }
-//        cir.cancel();
-//        cir.setReturnValue(OverlayTexture.pack(OverlayTexture.u(1), OverlayTexture.v(false)));
-//    }
 }
