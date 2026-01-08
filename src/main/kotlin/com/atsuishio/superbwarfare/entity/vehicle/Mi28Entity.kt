730fd130898cfc365402614af9816c19a269242c
Light_Quanta <zch22248@foxmail.com>
2026-01-08 20:20:47 +0800

    你知道吗，Mi28实体现在只有一行有效代码

--- src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/Mi28Entity.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/Mi28Entity.kt
@@ -4,5 +4,4 @@ import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.Level
 
-class Mi28Entity(type: EntityType<Mi28Entity>, world: Level) : GeoVehicleEntity(type, world) {
-}
+class Mi28Entity(type: EntityType<Mi28Entity>, world: Level) : GeoVehicleEntity(type, world)
