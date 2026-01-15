4ca29bd8ddfda7b23a20f09d014df8756ecc4e83
17146 <1714673995@qq.com>
2026-01-15 15:40:07 +0800

    修复mixin问题

--- src/main/java/com/atsuishio/superbwarfare/mixins/PlayerRendererMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/PlayerRendererMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
@@ -19,8 +20,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerRenderer.class)
 public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
-
-    // TODO 正确mixin
+    public PlayerRendererMixin(EntityRendererProvider.Context pContext, PlayerModel<AbstractClientPlayer> pModel, float pShadowRadius) {
+        super(pContext, pModel, pShadowRadius);
+    }
 
     @Shadow
     protected abstract void setModelProperties(AbstractClientPlayer pClientPlayer);
