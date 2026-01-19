35a6adc3591b63deb9c7dfae4a8c28593e0ead55
17146 <1714673995@qq.com>
2026-01-20 01:24:05 +0800

    调整文本，增加稀有度

--- src/main/java/com/atsuishio/superbwarfare/init/ModRarities.java
+++ src/main/java/com/atsuishio/superbwarfare/init/ModRarities.java
@@ -6,4 +6,6 @@ import net.minecraft.world.item.Rarity;
 public class ModRarities {
 
     public static final Rarity LEGENDARY = Rarity.create("superbwarfare_legendary", ChatFormatting.GOLD);
+    public static final Rarity SUPERB = Rarity.create("superbwarfare_superb", ChatFormatting.RED);
+    public static final Rarity VIRTUAL = Rarity.create("superbwarfare_virtual", s -> s.withColor(0xF4ADB4));
 }
