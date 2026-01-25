1c4d1b62252bbdae8388183a833bf8a3cd734ea5
17146 <1714673995@qq.com>
2026-01-25 18:11:44 +0800

    暂时移除numberValue接口

--- src/main/java/com/atsuishio/superbwarfare/data/gun/value/IntValue.kt
+++ src/main/java/com/atsuishio/superbwarfare/data/gun/value/IntValue.kt
@@ -1,21 +1,20 @@
 package com.atsuishio.superbwarfare.data.gun.value
 
-import com.atsuishio.superbwarfare.data.gun.value.base.NumberValue
 import net.minecraft.nbt.CompoundTag
 
 class IntValue(
     private val tag: CompoundTag,
     private val name: String,
-    override var defaultValue: Int = 0
-) : NumberValue<Int> {
-    override fun get(): Int {
+    val defaultValue: Int = 0
+) {
+    fun get(): Int {
         if (tag.contains(name)) {
             return tag.getInt(name)
         }
         return defaultValue
     }
 
-    override fun set(value: Int) {
+    fun set(value: Int) {
         if (value == defaultValue) {
             tag.remove(name)
         } else {
@@ -23,7 +22,11 @@ class IntValue(
         }
     }
 
-    override fun add(value: Int) {
+    fun add(value: Int) {
         set(get() + value)
+    }
+
+    fun reset() {
+        set(defaultValue)
     }
 }
--- src/main/java/com/atsuishio/superbwarfare/data/gun/value/base/NumberValue.kt
+++
@@ -1,5 +0,0 @@
-package com.atsuishio.superbwarfare.data.gun.value.base
-
-interface NumberValue<T : Number> : TagValue<T> {
-    fun add(value: T)
-}
