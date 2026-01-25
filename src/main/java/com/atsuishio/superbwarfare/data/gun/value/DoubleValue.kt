4566ee3d306666b1149476454cfb893e5a3ae483
17146 <1714673995@qq.com>
2026-01-25 18:11:44 +0800

    暂时移除numberValue接口

--- src/main/java/com/atsuishio/superbwarfare/data/gun/value/DoubleValue.kt
+++ src/main/java/com/atsuishio/superbwarfare/data/gun/value/DoubleValue.kt
@@ -1,22 +1,20 @@
 package com.atsuishio.superbwarfare.data.gun.value
 
-import com.atsuishio.superbwarfare.data.gun.value.base.NumberValue
 import net.minecraft.nbt.CompoundTag
 
 class DoubleValue(
     private val tag: CompoundTag,
     private val name: String,
-    override val defaultValue: Double = 0.0
-) : NumberValue<Double> {
-
-    override fun get(): Double {
+    val defaultValue: Double = 0.0
+) {
+    fun get(): Double {
         if (tag.contains(name)) {
             return tag.getDouble(name)
         }
         return defaultValue
     }
 
-    override fun set(value: Double) {
+    fun set(value: Double) {
         if (value == defaultValue) {
             tag.remove(name)
         } else {
@@ -24,7 +22,11 @@ class DoubleValue(
         }
     }
 
-    override fun add(value: Double) {
+    fun add(value: Double) {
         set(get() + value)
+    }
+
+    fun reset() {
+        set(defaultValue)
     }
 }
