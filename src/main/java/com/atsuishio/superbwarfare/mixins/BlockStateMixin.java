d33d976505b781860274a0fb9c8b4898194098ed
Atsuishio <842960157@qq.com>
2026-01-25 13:46:12 +0800

    龙牙一定要能阻挡载具！

--- src/main/java/com/atsuishio/superbwarfare/mixins/BlockStateMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/BlockStateMixin.java
@@ -1,10 +1,13 @@
 package com.atsuishio.superbwarfare.mixins;
 
 import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity;
+import com.atsuishio.superbwarfare.init.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour.BlockStateBase;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -12,12 +15,17 @@ import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockStateBase.class)
-public class BlockStateMixin {
+public abstract class BlockStateMixin {
+
+	@Shadow public abstract Block getBlock();
+
+	@Shadow protected abstract BlockState asState();
 
 	@Inject(at = @At("HEAD"), method = "getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", cancellable = true)
 	private void getCollisionShape(BlockGetter worldIn, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> ci) {
@@ -32,6 +40,10 @@ public class BlockStateMixin {
 
 			if (state.is(BlockTags.SWORD_EFFICIENT)) {
 				ci.setReturnValue(Shapes.empty());
+			}
+
+			if (state.is(ModBlocks.DRAGON_TEETH.get())) {
+				ci.setReturnValue(this.getBlock().getCollisionShape(this.asState(), worldIn, pos, context).move(0, Mth.clamp(vehicle.getBbHeight() - 0.25, 0, 1), 0));
 			}
 		}
 	}
