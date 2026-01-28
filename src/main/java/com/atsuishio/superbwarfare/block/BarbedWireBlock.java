d8ebf90692c4298ae626b8bdba8d3f20a2f59c5b
Atsuishio <842960157@qq.com>
2026-01-29 01:22:02 +0800

    调整人类模型的mixin，载具座位添加预制动作属性，优化部分载具下车位置

--- src/main/java/com/atsuishio/superbwarfare/block/BarbedWireBlock.java
+++ src/main/java/com/atsuishio/superbwarfare/block/BarbedWireBlock.java
@@ -1,6 +1,5 @@
 package com.atsuishio.superbwarfare.block;
 
-import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.registries.Registries;
@@ -8,6 +7,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -92,8 +92,7 @@ public class BarbedWireBlock extends Block {
     @Override
     public void entityInside(BlockState blockstate, Level world, BlockPos pos, Entity entity) {
         super.entityInside(blockstate, world, pos, entity);
-
-        if (!(entity instanceof VehicleEntity)) {
+        if (entity instanceof LivingEntity) {
             entity.makeStuckInBlock(Blocks.AIR.defaultBlockState(), new Vec3(0.15, 0.04, 0.15));
             entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.CACTUS)), 2);
         }
