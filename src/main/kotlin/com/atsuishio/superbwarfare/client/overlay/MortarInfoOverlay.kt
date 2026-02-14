6543287b89ce3f82a039c861e37a5a61b11ad13a
17146 <1714673995@qq.com>
2026-02-15 02:47:22 +0800

    处理部分todo，删除战术冲刺配置，调整投射物破坏方块配置名称

--- src/main/kotlin/com/atsuishio/superbwarfare/client/overlay/MortarInfoOverlay.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/overlay/MortarInfoOverlay.kt
@@ -1,8 +1,6 @@
 package com.atsuishio.superbwarfare.client.overlay
 
 import com.atsuishio.superbwarfare.client.overlay.components.CENTER
-import com.atsuishio.superbwarfare.client.overlay.components.LEFT_BOTTOM
-import com.atsuishio.superbwarfare.client.overlay.components.RIGHT_BOTTOM
 import com.atsuishio.superbwarfare.client.overlay.components.StringComponent
 import com.atsuishio.superbwarfare.entity.vehicle.MortarEntity
 import com.atsuishio.superbwarfare.tools.FormatTool.format1D
@@ -16,29 +14,13 @@ import net.minecraftforge.api.distmarker.OnlyIn
 
 @OnlyIn(Dist.CLIENT)
 object MortarInfoOverlay : CommonOverlay("mortar_info") {
-
     val BASE_POINT = CENTER.offset(-90F, -26F)
-
     val PITCH = StringComponent(BASE_POINT)
     val YAW = StringComponent(BASE_POINT.offsetY(10F))
     val RANGE = StringComponent(BASE_POINT.offsetY(20F))
 
-    // TODO 移除测试组件
-    val AAA = StringComponent(CENTER.offset(10F, -10F), LEFT_BOTTOM).apply {
-        component = Component.literal("AAAAAAAA")
-        color = 0xFF0000
-    }
-    val BBB = StringComponent(CENTER.offset(20F, 20F)).apply {
-        component = Component.literal("BBBBBBBB")
-        color = 0x00FF00
-    }
-    val CCC = StringComponent(RIGHT_BOTTOM.offset(-30F, -30F), RIGHT_BOTTOM).apply {
-        component = Component.literal("CCCCCCCC")
-        color = 0x0000FF
-    }
-
     init {
-        registerComponents(PITCH, YAW, RANGE, AAA, BBB, CCC)
+        registerComponents(PITCH, YAW, RANGE)
     }
 
     var mortar: MortarEntity? = null
--- src/main/kotlin/com/atsuishio/superbwarfare/config/server/ProjectileConfig.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/config/server/ProjectileConfig.kt
@@ -3,15 +3,12 @@ package com.atsuishio.superbwarfare.config.server
 import com.atsuishio.superbwarfare.config.buildServerConfig
 
 object ProjectileConfig {
-
-    // TODO 是否考虑重命名该属性？
     @JvmField
-    val BLOCK_DESTROY = buildServerConfig {
+    val PROJECTILE_DESTROY_BLOCKS = buildServerConfig {
         push("projectile")
 
         comment("Set true to allow projectiles to destroy certain blocks")
         define("allow_projectile_destroy_blocks", false)
             .also { pop() }
     }
-
 }
