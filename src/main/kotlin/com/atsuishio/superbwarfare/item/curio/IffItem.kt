ec5bcb8fd8486ce7f4c89418810120c2f57d7831
17146 <1714673995@qq.com>
2026-02-15 03:30:20 +0800

    kt化饰品

---
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/curio/IffItem.kt
@@ -0,0 +1,29 @@
+package com.atsuishio.superbwarfare.item.curio
+
+import net.minecraft.ChatFormatting
+import net.minecraft.network.chat.Component
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
+import top.theillusivec4.curios.api.CuriosApi
+import top.theillusivec4.curios.api.SlotContext
+import top.theillusivec4.curios.api.type.capability.ICurioItem
+import javax.annotation.ParametersAreNonnullByDefault
+
+class IffItem : Item(Properties().stacksTo(1)), ICurioItem {
+    override fun canEquip(slotContext: SlotContext, stack: ItemStack?): Boolean {
+        return CuriosApi.getCuriosInventory(slotContext.entity())
+            .flatMap { c -> c.findFirstCurio(this) }
+            .isEmpty
+    }
+
+    @ParametersAreNonnullByDefault
+    override fun appendHoverText(
+        stack: ItemStack,
+        context: TooltipContext,
+        tooltipComponents: MutableList<Component>,
+        tooltipFlag: TooltipFlag
+    ) {
+        tooltipComponents.add(Component.translatable("des.superbwarfare.iff_1").withStyle(ChatFormatting.GRAY))
+    }
+}
