0d0e674a395bcc15673676ac2221ebb086a80390
Atsuishio <842960157@qq.com>
2026-02-18 17:07:18 +0800

    添加工兵铲

---
+++ src/main/java/com/atsuishio/superbwarfare/item/MilitaryShovel.java
@@ -0,0 +1,48 @@
+package com.atsuishio.superbwarfare.item;
+
+import com.atsuishio.superbwarfare.Mod;
+import com.atsuishio.superbwarfare.client.renderer.item.MilitaryShovelRenderer;
+import com.atsuishio.superbwarfare.init.ModItems;
+import com.atsuishio.superbwarfare.tiers.ModItemTier;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ShovelItem;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.util.GeckoLibUtil;
+
+@EventBusSubscriber(modid = Mod.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class MilitaryShovel extends ShovelItem implements GeoItem {
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+
+    public MilitaryShovel(Properties pProperties) {
+        super(ModItemTier.STEEL, pProperties.rarity(Rarity.RARE).durability(500).attributes(createAttributes(ModItemTier.STEEL, 2F, -3F)));
+    }
+
+
+    @SubscribeEvent
+    public static void initializeClient(RegisterClientExtensionsEvent event) {
+        event.registerItem(new net.neoforged.neoforge.client.extensions.common.IClientItemExtensions() {
+            private final BlockEntityWithoutLevelRenderer renderer = new MilitaryShovelRenderer();
+
+            @Override
+            public @NotNull BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                return renderer;
+            }
+        }, ModItems.MILITARY_SHOVEL.get());
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar data) {
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return this.cache;
+    }
+}
