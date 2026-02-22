330d6da86b22a1b5f2cf8766d01765e57a4aa205
17146 <1714673995@qq.com>
2026-02-23 01:07:37 +0800

    kt化物品渲染类

---
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/item/Tm62ItemRenderer.kt
@@ -0,0 +1,7 @@
+package com.atsuishio.superbwarfare.client.renderer.item
+
+import com.atsuishio.superbwarfare.client.model.item.Tm62ItemModel
+import com.atsuishio.superbwarfare.item.Tm62Item
+import software.bernie.geckolib.renderer.GeoItemRenderer
+
+class Tm62ItemRenderer : GeoItemRenderer<Tm62Item>(Tm62ItemModel())
