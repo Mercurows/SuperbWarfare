91084097749d749c02705e84ee12895e6154c352
Atsuishio <842960157@qq.com>
2026-03-01 01:44:41 +0800

    加强下界合金大锤

--- src/main/kotlin/com/atsuishio/superbwarfare/item/weapon/NetheriteHammerItem.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/weapon/NetheriteHammerItem.kt
@@ -3,6 +3,6 @@ package com.atsuishio.superbwarfare.item.weapon
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Tiers
 
-class NetheriteHammerItem : HammerItem(Tiers.NETHERITE, 13, -3.2f, Properties().durability(2800).fireResistant()) {
+class NetheriteHammerItem : HammerItem(Tiers.NETHERITE, 75, -3.5f, Properties().durability(2800).fireResistant()) {
     override fun isDamageable(stack: ItemStack?) = false
 }
