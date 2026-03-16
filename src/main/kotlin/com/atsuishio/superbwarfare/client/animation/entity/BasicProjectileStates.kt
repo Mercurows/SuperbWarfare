217b19ec732a49b6980e3ae0a4881af6604c5dac
17146 <1714673995@qq.com>
2026-03-17 01:44:04 +0800

    kt化蜂群无人机渲染，调整投射物状态机

--- src/main/kotlin/com/atsuishio/superbwarfare/client/animation/entity/BasicProjectileStates.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/animation/entity/BasicProjectileStates.kt
@@ -15,10 +15,22 @@ object BasicProjectileStates {
             .evaluatePose { it.getPose() }
             .build()
 
-    val INIT_TRANS: SimpleTransition<BasicProjectileContext<*>> = SimpleTransition.Builder<BasicProjectileContext<*>>()
-        .predicate { true }
+    val START: SimpleAnimationState<BasicProjectileContext<*>> =
+        SimpleAnimationState.Builder<BasicProjectileContext<*>>()
+            .evaluatePose { it.getPose() }
+            .build()
+
+    val TO_IDLE: SimpleTransition<BasicProjectileContext<*>> = SimpleTransition.Builder<BasicProjectileContext<*>>()
+        .predicate { it.loop }
         .target(IDLE)
         .from(INIT)
-        .afterTrigger { it.playAnimation("animation.projectile.idle", AnimationPlayType.PLAY_ONCE_HOLD) }
+        .afterTrigger { it.playAnimation("animation.projectile.idle", AnimationPlayType.LOOP) }
+        .build()
+
+    val TO_START: SimpleTransition<BasicProjectileContext<*>> = SimpleTransition.Builder<BasicProjectileContext<*>>()
+        .predicate { !it.loop }
+        .target(START)
+        .from(INIT)
+        .afterTrigger { it.playAnimation("animation.projectile.start", AnimationPlayType.PLAY_ONCE_HOLD) }
         .build()
 }
