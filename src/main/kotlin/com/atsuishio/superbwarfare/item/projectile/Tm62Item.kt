7992eb460ed278579d708646c73dfbb4bfcda377
17146 <1714673995@qq.com>
2026-03-21 16:41:30 +0800

    尝试使用sbm渲染物品模型

--- src/main/kotlin/com/atsuishio/superbwarfare/item/projectile/Tm62Item.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/projectile/Tm62Item.kt
@@ -4,6 +4,7 @@ import com.atsuishio.superbwarfare.client.renderer.item.Tm62ItemRenderer
 import com.atsuishio.superbwarfare.entity.Tm62Entity
 import com.atsuishio.superbwarfare.init.ModEntities
 import com.atsuishio.superbwarfare.item.DispenserLaunchable
+import com.atsuishio.superbwarfare.tools.mc
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer
 import net.minecraft.core.BlockSource
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior
@@ -18,31 +19,21 @@ import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.DispenserBlock
 import net.minecraftforge.client.extensions.common.IClientItemExtensions
 import org.joml.Math
-import software.bernie.geckolib.animatable.GeoItem
-import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache
-import software.bernie.geckolib.core.animation.AnimatableManager.ControllerRegistrar
-import software.bernie.geckolib.util.GeckoLibUtil
 import java.util.function.Consumer
 
-open class Tm62Item : Item(Properties().stacksTo(8)), GeoItem, DispenserLaunchable {
-    private val cache: AnimatableInstanceCache = GeckoLibUtil.createInstanceCache(this)
-
+open class Tm62Item : Item(Properties().stacksTo(8)), DispenserLaunchable {
     override fun initializeClient(consumer: Consumer<IClientItemExtensions>) {
         super.initializeClient(consumer)
         consumer.accept(object : IClientItemExtensions {
-            private val renderer: BlockEntityWithoutLevelRenderer = Tm62ItemRenderer()
+            private var renderer: BlockEntityWithoutLevelRenderer? = null
 
             override fun getCustomRenderer(): BlockEntityWithoutLevelRenderer {
-                return renderer
+                if (renderer == null) {
+                    renderer = Tm62ItemRenderer(mc.blockEntityRenderDispatcher, mc.entityModels)
+                }
+                return renderer!!
             }
         })
-    }
-
-    override fun registerControllers(data: ControllerRegistrar?) {
-    }
-
-    override fun getAnimatableInstanceCache(): AnimatableInstanceCache {
-        return this.cache
     }
 
     override fun use(level: Level, player: Player, hand: InteractionHand): InteractionResultHolder<ItemStack> {
