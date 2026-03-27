b8952f8098314fb63f4f16725f06c0fb8b63ddce
17146 <1714673995@qq.com>
2026-03-27 20:19:55 +0800

    修复config问题

--- src/main/kotlin/com/atsuishio/superbwarfare/config/server/MiscConfig.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/config/server/MiscConfig.kt
@@ -98,7 +98,7 @@ object MiscConfig {
     @JvmField
     val SYNC_ENTITY_INTERVAL = buildServerConfig {
         comment("The interval for synchronizing client entities with the server")
-        define("sync_entity_interval", 3)
+        defineInRange("sync_entity_interval", 3, 1, Int.MAX_VALUE)
     }
 
     @JvmField
