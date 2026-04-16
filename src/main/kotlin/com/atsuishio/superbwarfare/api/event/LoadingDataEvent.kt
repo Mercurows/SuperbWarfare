fb01916a82914aaeabdf2f461da871b475f792e0
17146 <1714673995@qq.com>
2026-04-16 22:26:02 +0800

    调整1.20的event写法

--- src/main/kotlin/com/atsuishio/superbwarfare/api/event/LoadingDataEvent.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/api/event/LoadingDataEvent.kt
@@ -2,9 +2,11 @@ package com.atsuishio.superbwarfare.api.event
 
 import com.atsuishio.superbwarfare.data.gun.DefaultGunData
 import com.atsuishio.superbwarfare.data.vehicle.DefaultVehicleData
+import net.minecraftforge.eventbus.api.Cancelable
 import net.minecraftforge.eventbus.api.Event
 import org.jetbrains.annotations.ApiStatus
 
+@Cancelable
 @ApiStatus.AvailableSince("0.8.9")
 open class LoadingDataEvent<T : Any> private constructor(
     val id: String,
--- src/main/kotlin/com/atsuishio/superbwarfare/api/event/LoadingJsonEvent.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/api/event/LoadingJsonEvent.kt
@@ -4,11 +4,13 @@ import com.google.gson.JsonParser
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
+import net.minecraftforge.eventbus.api.Cancelable
 import net.minecraftforge.eventbus.api.Event
 import org.jetbrains.annotations.ApiStatus
 
 typealias GsonObject = com.google.gson.JsonObject
 
+@Cancelable
 @ApiStatus.AvailableSince("0.8.9")
 open class LoadingJsonEvent(
     val id: String,
