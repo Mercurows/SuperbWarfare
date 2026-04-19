fffd44697394b95ce4ec3928f689750618c145cd
Atsuishio <842960157@qq.com>
2026-04-19 21:21:32 +0800

    翻新快艇模型

--- src/main/kotlin/com/atsuishio/superbwarfare/client/model/entity/SpeedboatModel.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/model/entity/SpeedboatModel.kt
@@ -4,10 +4,23 @@ import com.atsuishio.superbwarfare.entity.vehicle.SpeedboatEntity
 import net.minecraft.util.Mth
 
 class SpeedboatModel : VehicleModel<SpeedboatEntity>() {
+    override fun hideForTurretControllerWhileZooming() = true
     override fun collectTransform(boneName: String): TransformContext<SpeedboatEntity>? {
         if (boneName == "propeller") {
             return TransformContext { bone, vehicle, state ->
                 bone.rotZ = Mth.lerp(state.partialTick, vehicle.propellerRotO, vehicle.propellerRot)
+            }
+        }
+
+        if (boneName == "propeller2") {
+            return TransformContext { bone, vehicle, state ->
+                bone.rotZ = -Mth.lerp(state.partialTick, vehicle.propellerRotO, vehicle.propellerRot)
+            }
+        }
+
+        if (boneName == "control") {
+            return TransformContext { bone, vehicle, state ->
+                bone.rotZ = -4 * Mth.lerp(state.partialTick, vehicle.rudderRotO, vehicle.rudderRot)
             }
         }
 
