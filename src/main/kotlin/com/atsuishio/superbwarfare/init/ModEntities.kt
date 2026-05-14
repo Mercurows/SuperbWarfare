a4cca120bce7fcd23c369faf905e4c8e0bd42fb8
17146 <1714673995@qq.com>
2026-05-15 01:32:34 +0800

    回调先辈身高

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModEntities.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModEntities.kt
@@ -44,7 +44,7 @@ object ModEntities {
     val SENPAI = register(
         "senpai",
         EntityType.Builder.of(::SenpaiEntity, MobCategory.MONSTER)
-            .setTrackingRange(64).setUpdateInterval(3).sized(0.65f, 1.7f)
+            .setTrackingRange(64).setUpdateInterval(3).sized(0.65f, 2f)
     )
 
     @JvmField
