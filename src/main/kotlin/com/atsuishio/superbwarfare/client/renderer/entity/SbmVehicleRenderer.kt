fd076b70fc761cb2a92f3bb0899cfa9a77502368
Atsuishio <842960157@qq.com>
2026-05-18 14:38:50 +0800

    尝试给sbm载具加入轮胎模型旋转功能

--- src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/SbmVehicleRenderer.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/SbmVehicleRenderer.kt
@@ -1,8 +1,10 @@
 package com.atsuishio.superbwarfare.client.renderer.entity
 
 import com.atsuishio.superbwarfare.Mod.Companion.loc
+import com.atsuishio.superbwarfare.client.model.entity.VehicleModel
 import com.atsuishio.superbwarfare.entity.vehicle.BasicGeoVehicleEntity
 import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
+import com.atsuishio.superbwarfare.event.ClientEventHandler
 import com.atsuishio.superbwarfare.resource.BedrockModelLoader
 import com.github.mcmodderanchor.simplebedrockmodel.v1.client.renderer.BedrockModelRenderTypes
 import com.github.mcmodderanchor.simplebedrockmodel.v1.common.model.BedrockModel
@@ -12,6 +14,7 @@ import com.maydaymemory.mae.blend.EulerAdditiveBlender
 import com.maydaymemory.mae.blend.SimpleEulerAdditiveBlender
 import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.math.Axis
+import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.entity.EntityRenderer
@@ -20,9 +23,29 @@ import net.minecraft.client.renderer.texture.OverlayTexture
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.util.Mth
 import net.minecraft.world.phys.Vec3
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 open class SbmVehicleRenderer<T>(manager: EntityRendererProvider.Context) :
     EntityRenderer<T>(manager) where T : VehicleEntity, T : BasicGeoVehicleEntity {
+
+    protected var pitch = 0f
+    protected var yaw = 0f
+    protected var roll = 0f
+    protected var leftWheelRot = 0f
+    protected var rightWheelRot = 0f
+    protected var leftTrack = 0f
+    protected var rightTrack = 0f
+
+    protected var turretYRot = 0f
+
+    protected var turretXRot = 0f
+    protected var turretYaw = 0f
+    protected var recoilShake = 0f
+
+    protected var hideForTurretControllerWhileZooming = false
+    protected var hideForPassengerWeaponStationControllerWhileZooming = false
+
     override fun getTextureLocation(entity: T): ResourceLocation {
         return loc("textures/bedrock/vehicle/${entity.type.descriptionId.split(".")[2]}.png")
     }
@@ -78,7 +101,44 @@ open class SbmVehicleRenderer<T>(manager: EntityRendererProvider.Context) :
         poseStack.popPose()
     }
 
-    open fun customModelPart(entityIn: T, model: BedrockModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float) {
+    open fun customModelPart(vehicle: T, model: BedrockModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float) {
+        pitch = vehicle.getPitch(partialTicks)
+        yaw = vehicle.getYaw(partialTicks)
+        roll = vehicle.getRoll(partialTicks)
+
+        leftWheelRot = Mth.lerp(partialTicks, vehicle.leftWheelRotO, vehicle.leftWheelRot)
+        rightWheelRot = Mth.lerp(partialTicks, vehicle.rightWheelRotO, vehicle.rightWheelRot)
+
+        leftTrack = Mth.lerp(partialTicks, vehicle.leftTrackO, vehicle.leftTrack)
+        rightTrack = Mth.lerp(partialTicks, vehicle.rightTrackO, vehicle.rightTrack)
+
+        turretYRot = Mth.lerp(partialTicks, vehicle.turretYRotO, vehicle.turretYRot)
+        turretXRot = Mth.lerp(partialTicks, vehicle.turretXRotO, vehicle.turretXRot)
+
+        turretYaw = vehicle.getTurretYaw(partialTicks)
+
+        recoilShake = Mth.lerp(partialTicks, vehicle.recoilShakeO.toFloat(), vehicle.recoilShake.toFloat())
+
+        hideForTurretControllerWhileZooming =
+            ClientEventHandler.zoomVehicle && vehicle.getNthEntity(vehicle.turretControllerIndex) === Minecraft.getInstance().player
+        hideForPassengerWeaponStationControllerWhileZooming =
+            ClientEventHandler.zoomVehicle && vehicle.getNthEntity(vehicle.passengerWeaponStationControllerIndex) === Minecraft.getInstance().player
+
+
+        val wheelMatcher: Matcher = VehicleModel.WHEEL_PATTERN.matcher(boneName)
+        if (wheelMatcher.matches()) {
+            val isL = wheelMatcher.group("direction") == "L"
+
+            return if (boneName.endsWith("Turn")) {
+                TransformContext { bone, vehicle, state ->
+                    bone.rotX = 1.5f * (if (isL) leftWheelRot else rightWheelRot)
+                    bone.rotY = Mth.lerp(state.partialTick, vehicle.rudderRotO, vehicle.rudderRot)
+                }
+            } else {
+                TransformContext { bone, _, _ -> bone.rotX = 1.5f * (if (isL) leftWheelRot else rightWheelRot) }
+            }
+        }
+
     }
 
     open fun vehicleAxis(entityIn: T, poseStack: PoseStack, entityYaw: Float, partialTicks: Float) {
@@ -105,5 +165,6 @@ open class SbmVehicleRenderer<T>(manager: EntityRendererProvider.Context) :
 
     companion object {
         val BLENDER: EulerAdditiveBlender = SimpleEulerAdditiveBlender(ZYXBoneTransformFactory()) { ArrayPoseBuilder() }
+        val WHEEL_PATTERN: Pattern = Pattern.compile("^wheel(?<direction>[LR]).*$")
     }
 }
