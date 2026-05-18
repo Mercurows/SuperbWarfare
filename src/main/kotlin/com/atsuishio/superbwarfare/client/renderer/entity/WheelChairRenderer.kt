eae7b3581436e55cf2adca93ea052e4a886acadf
Atsuishio <842960157@qq.com>
2026-05-18 14:29:10 +0800

    轮椅替换为SBM渲染

--- src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/WheelChairRenderer.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/WheelChairRenderer.kt
@@ -1,13 +1,24 @@
 package com.atsuishio.superbwarfare.client.renderer.entity
 
-import com.atsuishio.superbwarfare.client.model.entity.WheelChairModel
-import com.atsuishio.superbwarfare.entity.vehicle.WheelChairEntity
+import com.atsuishio.superbwarfare.entity.vehicle.BasicGeoVehicleEntity
+import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
+import com.github.mcmodderanchor.simplebedrockmodel.v1.common.model.BedrockModel
+import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.renderer.entity.EntityRendererProvider
+import net.minecraft.util.Mth
 
-class WheelChairRenderer(renderManager: EntityRendererProvider.Context) :
-    VehicleRenderer<WheelChairEntity>(renderManager, WheelChairModel()) {
+open class WheelChairRenderer<T>(manager: EntityRendererProvider.Context) :
+    SbmVehicleRenderer<T>(manager) where T : VehicleEntity, T : BasicGeoVehicleEntity {
 
-    init {
-        this.shadowRadius = 0.5f
+    override fun customModelPart(entityIn: T, model: BedrockModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float) {
+        val wRb = model.getBone("w_rb")
+        val wLb = model.getBone("w_lb")
+        val wRr = model.getBone("w_rr")
+        val wLr = model.getBone("w_lr")
+
+        wRb.rotation.rotationX(Mth.lerp(partialTicks, entityIn.rightWheelRotO, entityIn.rightWheelRot))
+        wLb.rotation.rotationX(Mth.lerp(partialTicks, entityIn.leftWheelRotO, entityIn.leftWheelRot))
+        wRr.rotation.rotationX(4 * Mth.lerp(partialTicks, entityIn.rightWheelRotO, entityIn.rightWheelRot))
+        wLr.rotation.rotationX(4 * Mth.lerp(partialTicks, entityIn.leftWheelRotO, entityIn.leftWheelRot))
     }
 }
