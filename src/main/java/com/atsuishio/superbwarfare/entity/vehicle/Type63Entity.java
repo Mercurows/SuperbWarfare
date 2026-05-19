3b52c9eac7fd5149b4a6ceca26c8481a820c7a5e
Atsuishio <842960157@qq.com>
2026-05-19 23:43:27 +0800

    sbm化63式火箭炮

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Type63Entity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/Type63Entity.java
@@ -1,8 +1,9 @@
 package com.atsuishio.superbwarfare.entity.vehicle;
 
+import com.atsuishio.superbwarfare.client.animation.entity.BasicProjectileAnimationInstance;
 import com.atsuishio.superbwarfare.data.gun.GunProp;
 import com.atsuishio.superbwarfare.entity.projectile.MediumRocketEntity;
-import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity;
+import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity;
 import com.atsuishio.superbwarfare.entity.vehicle.utils.VehicleVecUtils;
 import com.atsuishio.superbwarfare.init.*;
 import com.atsuishio.superbwarfare.item.projectile.MediumRocketItem;
@@ -14,6 +15,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
@@ -29,13 +31,14 @@ import net.minecraft.world.level.entity.EntityTypeTest;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
-import org.joml.*;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Math;
+import org.joml.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Type63Entity extends GeoVehicleEntity {
+public class Type63Entity extends VehicleEntity implements BasicGeoVehicleEntity {
 
     public static final EntityDataAccessor<Float> TARGET_PITCH = SynchedEntityData.defineId(Type63Entity.class, EntityDataSerializers.FLOAT);
     public static final EntityDataAccessor<Float> TARGET_YAW = SynchedEntityData.defineId(Type63Entity.class, EntityDataSerializers.FLOAT);
@@ -473,5 +476,23 @@ public class Type63Entity extends GeoVehicleEntity {
             }
         }
         this.entityData.set(LOADED_AMMO, list);
+    }
+
+    @Nullable
+    @Override
+    public ResourceLocation getAnimation() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public BasicProjectileAnimationInstance<?> getAnimationInstance() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public ResourceLocation getEmissiveTexture() {
+        return null;
     }
 }
