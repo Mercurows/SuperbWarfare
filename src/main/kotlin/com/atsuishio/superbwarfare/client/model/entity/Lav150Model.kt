5acfa4f7ce52f0c57da072ea4c48d38c80fcde82
Atsuishio <842960157@qq.com>
2026-05-19 14:48:43 +0800

    移植炮塔逻辑，sbm化lav150

--- src/main/kotlin/com/atsuishio/superbwarfare/client/model/entity/Lav150Model.kt
+++
@@ -1,7 +0,0 @@
-package com.atsuishio.superbwarfare.client.model.entity
-
-import com.atsuishio.superbwarfare.entity.vehicle.Lav150Entity
-
-class Lav150Model : VehicleModel<Lav150Entity>() {
-    override fun hideForTurretControllerWhileZooming() = true
-}
