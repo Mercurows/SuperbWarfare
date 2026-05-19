2923d684a9a61bb6acd215d03e8ee9dcd85e30c7
Atsuishio <842960157@qq.com>
2026-05-19 15:38:21 +0800

    sbm化lav25

--- src/main/kotlin/com/atsuishio/superbwarfare/client/model/entity/Lav25Model.kt
+++
@@ -1,7 +0,0 @@
-package com.atsuishio.superbwarfare.client.model.entity
-
-import com.atsuishio.superbwarfare.entity.vehicle.Lav25Entity
-
-class Lav25Model : VehicleModel<Lav25Entity>() {
-    override fun hideForTurretControllerWhileZooming() = true
-}
