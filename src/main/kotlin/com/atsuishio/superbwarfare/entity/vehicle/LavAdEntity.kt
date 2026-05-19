d7e9a2fb9a8b9c46b0ddbe659ebd044a0ddbcf54
Atsuishio <842960157@qq.com>
2026-05-19 16:01:52 +0800

    sbm化lavAd

--- src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/LavAdEntity.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/LavAdEntity.kt
@@ -1,10 +1,10 @@
 package com.atsuishio.superbwarfare.entity.vehicle
 
-import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity
+import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.Level
 
-class LavAdEntity(type: EntityType<LavAdEntity>, world: Level) : GeoVehicleEntity(type, world) {
+class LavAdEntity(type: EntityType<LavAdEntity>, world: Level) : VehicleEntity(type, world), BasicGeoVehicleEntity {
 
     override fun getDamageModifier() = super.getDamageModifier()
         .custom { source, damage -> getSourceAngle(source, 0.15f) * damage }
