db9a1ecaf8d4ddd1aa2a2986d162f0c3a1295dd7
Atsuishio <842960157@qq.com>
2026-05-19 23:45:21 +0800

    修复方法错误

--- src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/Type63Renderer.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/Type63Renderer.kt
@@ -4,14 +4,13 @@ import com.atsuishio.superbwarfare.client.model.entity.BedrockVehicleModel
 import com.atsuishio.superbwarfare.entity.vehicle.BasicGeoVehicleEntity
 import com.atsuishio.superbwarfare.entity.vehicle.Type63Entity
 import com.mojang.blaze3d.vertex.PoseStack
-import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 
 class Type63Renderer<T>(manager: EntityRendererProvider.Context) :
     SbmVehicleRenderer<T>(manager) where T : Type63Entity, T : BasicGeoVehicleEntity {
 
-    override fun transformCustomModelPart(vehicle: T, model: BedrockVehicleModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float, buffer: MultiBufferSource, packedLight: Int) {
-        super.transformCustomModelPart(vehicle, model, poseStack, entityYaw, partialTicks, buffer, packedLight)
+    override fun transformCustomModelPart(vehicle: T, model: BedrockVehicleModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float) {
+        super.transformCustomModelPart(vehicle, model, poseStack, entityYaw, partialTicks)
         val shouLunX = model.getBone("shoulunx")
         val shouLunY = model.getBone("shouluny")
 
