baa9516c658ca8d26800b8f68dbd7493b52a3d7f
Atsuishio <842960157@qq.com>
2026-05-19 23:11:16 +0800

    sbm化Tom6和Tow

--- src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/Ah6Renderer.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/Ah6Renderer.kt
@@ -4,15 +4,16 @@ import com.atsuishio.superbwarfare.client.model.entity.BedrockVehicleModel
 import com.atsuishio.superbwarfare.entity.vehicle.BasicGeoVehicleEntity
 import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
 import com.mojang.blaze3d.vertex.PoseStack
+import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.util.Mth
 
 class Ah6Renderer<T>(manager: EntityRendererProvider.Context) :
     SbmVehicleRenderer<T>(manager) where T : VehicleEntity, T : BasicGeoVehicleEntity {
-    override fun transformCustomModelPart(vehicle: T, model: BedrockVehicleModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float) {
+    override fun transformCustomModelPart(vehicle: T, model: BedrockVehicleModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float, buffer: MultiBufferSource, packedLight: Int) {
 
         // TODO 实现lod功能
-        super.transformCustomModelPart(vehicle, model, poseStack, entityYaw, partialTicks)
+        super.transformCustomModelPart(vehicle, model, poseStack, entityYaw, partialTicks, buffer, packedLight)
         val propeller = model.getBone("propeller")
         val tailPropeller = model.getBone("tailPropeller")
 
--- src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/WheelChairRenderer.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/entity/WheelChairRenderer.kt
@@ -4,13 +4,14 @@ import com.atsuishio.superbwarfare.client.model.entity.BedrockVehicleModel
 import com.atsuishio.superbwarfare.entity.vehicle.BasicGeoVehicleEntity
 import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
 import com.mojang.blaze3d.vertex.PoseStack
+import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.util.Mth
 
 open class WheelChairRenderer<T>(manager: EntityRendererProvider.Context) :
     SbmVehicleRenderer<T>(manager) where T : VehicleEntity, T : BasicGeoVehicleEntity {
 
-    override fun transformCustomModelPart(vehicle: T, model: BedrockVehicleModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float) {
+    override fun transformCustomModelPart(vehicle: T, model: BedrockVehicleModel, poseStack: PoseStack, entityYaw: Float, partialTicks: Float, buffer: MultiBufferSource, packedLight: Int) {
         val wRb = model.getBone("w_rb")
         val wLb = model.getBone("w_lb")
         val wRr = model.getBone("w_rr")
