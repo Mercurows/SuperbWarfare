f915fc82f2084ad9abf079fe72382ab980b72db0
Atsuishio <842960157@qq.com>
2026-05-20 16:40:27 +0800

    sbm化泥头车

--- src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/TruckEntity.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/TruckEntity.kt
@@ -2,7 +2,7 @@ package com.atsuishio.superbwarfare.entity.vehicle
 
 import com.atsuishio.superbwarfare.entity.getValue
 import com.atsuishio.superbwarfare.entity.setValue
-import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity
+import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.syncher.EntityDataAccessor
 import net.minecraft.network.syncher.EntityDataSerializers
@@ -15,7 +15,7 @@ import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.Level
 
-class TruckEntity(type: EntityType<TruckEntity>, world: Level) : GeoVehicleEntity(type, world) {
+class TruckEntity(type: EntityType<TruckEntity>, world: Level) : VehicleEntity(type, world), BasicGeoVehicleEntity {
 
     override fun defineSynchedData() {
         super.defineSynchedData()
