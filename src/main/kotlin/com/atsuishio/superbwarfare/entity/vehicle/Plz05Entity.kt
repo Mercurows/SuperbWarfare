b6759cd7cc5ffaf26803e5245665f862935e5d56
Atsuishio <842960157@qq.com>
2026-05-22 23:55:30 +0800

    sbm化PLZ05

--- src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/Plz05Entity.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/entity/vehicle/Plz05Entity.kt
@@ -1,8 +1,5 @@
 package com.atsuishio.superbwarfare.entity.vehicle
 
-import com.atsuishio.superbwarfare.Mod
-import com.atsuishio.superbwarfare.client.animation.AnimationPlayType
-import com.atsuishio.superbwarfare.client.animation.entity.VehicleAnimationInstance
 import com.atsuishio.superbwarfare.entity.vehicle.base.ArtilleryEntity
 import com.atsuishio.superbwarfare.tools.angleTo
 import com.atsuishio.superbwarfare.tools.toVec3
@@ -10,17 +7,6 @@ import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.Level
 
 open class Plz05Entity(type: EntityType<Plz05Entity>, world: Level) : ArtilleryEntity(type, world), BasicGeoVehicleEntity {
-
-    val anim: VehicleAnimationInstance<Plz05Entity>? =
-        if (world.isClientSide) VehicleAnimationInstance(this) else null
-    override fun getAnimationInstance() = anim
-    override fun getAnimation() = ANIM
-    companion object {
-        val ANIM = Mod.loc("animation/bedrock/vehicle/plz_05.animation.json")
-    }
-
-    private var wasLockTurret = false
-
     override fun baseTick() {
         super.baseTick()
 
@@ -34,19 +20,24 @@ open class Plz05Entity(type: EntityType<Plz05Entity>, world: Level) : ArtilleryE
         } else {
             lockTurret = false
         }
-
-        if (level().isClientSide) {
-            val ctx = anim?.context ?: return
-            if (lockTurret && !wasLockTurret) {
-                ctx.playAnimation("animation.plz_05.lock_turret", AnimationPlayType.LOOP,
-                    fadeInTicks = 40)
-            } else if (!lockTurret && wasLockTurret) {
-                ctx.stopAnimation("animation.plz_05.lock_turret",
-                    fadeOutTicks = 80)
-            }
-            wasLockTurret = lockTurret
-        }
     }
+
+//    override fun registerControllers(data: AnimatableManager.ControllerRegistrar) = buildControllers(data) {
+//        "shoot" {
+//            if (getShootAnimationTimer(1, 0) > 0) {
+//                thenPlay("animation.plz_05.shoot")
+//            } else {
+//                thenLoop("animation.plz_05.idle")
+//            }
+//        }
+//        "lockTurret"(10) {
+//            if (lockTurret) {
+//                thenPlay("animation.plz_05.lock_turret")
+//            } else {
+//                thenLoop("animation.plz_05.idle")
+//            }
+//        }
+//    }
 
     override fun canBind() = true
 }
