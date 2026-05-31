51b0b97103438d19482dfa10f6627d419b0ceb38
Atsuishio <842960157@qq.com>
2026-05-31 16:06:01 +0800

    集装箱图标替换为载具图标

--- src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/item/ContainerBlockItemRenderer.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/item/ContainerBlockItemRenderer.kt
@@ -1,13 +1,18 @@
 package com.atsuishio.superbwarfare.client.renderer.item
 
+import com.atsuishio.superbwarfare.client.decorator.ContainerItemDecorator
 import com.atsuishio.superbwarfare.client.model.item.ContainerItemModel
 import com.atsuishio.superbwarfare.item.container.ContainerBlockItem
+import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.ItemDisplayContext
+import net.minecraft.world.item.ItemStack
 import software.bernie.geckolib.renderer.GeoItemRenderer
 
 class ContainerBlockItemRenderer : GeoItemRenderer<ContainerBlockItem>(ContainerItemModel()) {
+
     override fun getRenderType(
         animatable: ContainerBlockItem,
         texture: ResourceLocation?,
@@ -15,5 +20,20 @@ class ContainerBlockItemRenderer : GeoItemRenderer<ContainerBlockItem>(Container
         partialTick: Float
     ): RenderType {
         return RenderType.entityTranslucent(getTextureLocation(animatable))
+    }
+
+    override fun renderByItem(
+        stack: ItemStack,
+        transformType: ItemDisplayContext,
+        poseStack: PoseStack,
+        bufferSource: MultiBufferSource,
+        packedLight: Int,
+        packedOverlay: Int
+    ) {
+        // GUI模式下有自定义图标时，隐藏3D集装箱模型，由ContainerItemDecorator绘制2D图标
+        if (transformType == ItemDisplayContext.GUI && ContainerItemDecorator.getCustomIcon(stack) != null) {
+            return
+        }
+        super.renderByItem(stack, transformType, poseStack, bufferSource, packedLight, packedOverlay)
     }
 }
