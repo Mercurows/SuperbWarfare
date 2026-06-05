2dd6215bb03afffb2cbcd18ff14468156ee7307f
Light_Quanta <zch22248@foxmail.com>
2026-06-06 01:21:06 +0800

    添加HealthAmmoStrategy

---
+++ src/main/java/com/atsuishio/superbwarfare/data/gun/ammo_consumer_strategy/HealthAmmoStrategy.kt
@@ -0,0 +1,55 @@
+package com.atsuishio.superbwarfare.data.gun.ammo_consumer_strategy
+
+import com.atsuishio.superbwarfare.data.gun.AmmoConsumer
+import com.atsuishio.superbwarfare.data.gun.GunData
+import com.atsuishio.superbwarfare.init.ModDamageTypes
+import com.atsuishio.superbwarfare.tools.forceHurt
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.LivingEntity
+import net.neoforged.neoforge.items.IItemHandler
+import kotlin.math.floor
+
+/**
+ * health
+ * health 10 (每1血=10弹药)
+ */
+object HealthAmmoStrategy : AmmoConsumeStrategy() {
+
+    override val defaultType = AmmoConsumer.AmmoConsumeType.ITEM
+
+    var ammoPerHealth: Float = 1F
+
+    override fun match(ammo: String) =
+        ammo.startsWith("health", true)
+                && ammo == "health" || ammo.substringAfter("health").trimEnd().toFloatOrNull() != null
+
+    override fun init(
+        consumer: AmmoConsumer,
+        count: Int,
+        matchedString: String
+    ) {
+        super.init(consumer, count, matchedString)
+        val extracted = matchedString.substringAfter("health").trimEnd().toFloatOrNull()?.coerceAtLeast(0F) ?: 1F
+        ammoPerHealth = if (extracted.isNaN() || extracted == 0F || extracted.isInfinite()) 1F else extracted
+    }
+
+    override fun consume(data: GunData, consumer: AmmoConsumer, shooter: Entity, count: Int): Int {
+        shooter.forceHurt(
+            ModDamageTypes.causeMineDamage(shooter.level().registryAccess(), shooter),
+            count / ammoPerHealth
+        )
+        return 1
+    }
+
+    override fun consume(data: GunData, consumer: AmmoConsumer, handler: IItemHandler, count: Int) = 0
+    override fun count(data: GunData, consumer: AmmoConsumer, entity: Entity?) =
+        floor(((entity as? LivingEntity)?.health ?: 0F) * ammoPerHealth - 0.00001).toInt()
+
+    override fun count(data: GunData, consumer: AmmoConsumer, handler: IItemHandler?) = 0
+    override fun withdraw(consumer: AmmoConsumer, ammoSupplier: Entity, count: Int): Int {
+        (ammoSupplier as? LivingEntity)?.heal(count / ammoPerHealth) ?: return 0
+        return count
+    }
+
+    override fun withdraw(consumer: AmmoConsumer, handler: IItemHandler, count: Int) = 0
+}
