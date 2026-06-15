c1e9715c2555909b4f71a3f3ec42dcee5f6179a7
17146 <1714673995@qq.com>
2026-06-15 19:59:01 +0800

    sbm化载具装配台方块

--- src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/item/VehicleAssemblingTableBlockItemRenderer.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/item/VehicleAssemblingTableBlockItemRenderer.kt
@@ -1,20 +1,49 @@
 package com.atsuishio.superbwarfare.client.renderer.item
 
-import com.atsuishio.superbwarfare.client.model.item.VehicleAssemblingTableItemModel
+import com.atsuishio.superbwarfare.Mod.Companion.loc
 import com.atsuishio.superbwarfare.item.blockitem.VehicleAssemblingTableBlockItem
+import com.atsuishio.superbwarfare.resource.model.BlockModelReloadListener
+import com.mojang.blaze3d.vertex.PoseStack
+import net.minecraft.client.model.geom.EntityModelSet
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.RenderType
-import net.minecraft.resources.ResourceLocation
-import software.bernie.geckolib.renderer.GeoItemRenderer
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher
+import net.minecraft.world.item.ItemDisplayContext
+import net.minecraft.world.item.ItemStack
 
-class VehicleAssemblingTableBlockItemRenderer :
-    GeoItemRenderer<VehicleAssemblingTableBlockItem>(VehicleAssemblingTableItemModel()) {
-    override fun getRenderType(
-        animatable: VehicleAssemblingTableBlockItem,
-        texture: ResourceLocation?,
-        bufferSource: MultiBufferSource?,
-        partialTick: Float
-    ): RenderType {
-        return RenderType.entityTranslucent(getTextureLocation(animatable))
+class VehicleAssemblingTableBlockItemRenderer(dispatcher: BlockEntityRenderDispatcher, set: EntityModelSet) :
+    BlockEntityWithoutLevelRenderer(dispatcher, set) {
+    override fun renderByItem(
+        stack: ItemStack,
+        transformType: ItemDisplayContext,
+        poseStack: PoseStack,
+        bufferSource: MultiBufferSource,
+        packedLight: Int,
+        packedOverlay: Int
+    ) {
+        if (stack.item !is VehicleAssemblingTableBlockItem) return
+
+        val model = BlockModelReloadListener.getModel(MODEL) ?: return
+
+        poseStack.pushPose()
+
+        poseStack.translate(0.5f, 0.5f, 0.5f)
+
+        model.applyPose(model.bindPose)
+
+        model.renderToBuffer(
+            poseStack,
+            bufferSource.getBuffer(RenderType.entityCutout(TEXTURE)),
+            packedLight,
+            packedOverlay
+        )
+
+        poseStack.popPose()
+    }
+
+    companion object {
+        val TEXTURE = loc("textures/bedrock/block/vehicle_assembling_table.png")
+        val MODEL = loc("models/bedrock/block/vehicle_assembling_table.geo.json")
     }
 }
