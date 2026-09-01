937462f68254163da87281f1677e3f30ba19a443
Atsuishio <842960157@qq.com>
2026-09-01 19:08:07 +0800

    优化马林的模型贴图并添加一个瞄准镜的镜桥

--- src/main/kotlin/com/atsuishio/superbwarfare/item/gun/rifle/MarlinItem.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/gun/rifle/MarlinItem.kt
@@ -1,9 +1,13 @@
 package com.atsuishio.superbwarfare.item.gun.rifle
 
+import com.atsuishio.superbwarfare.data.gun.GunData
 import com.atsuishio.superbwarfare.item.gun.GeoGunItemV2
 
 object MarlinItem : GeoGunItemV2(Properties()) {
-
+    override fun hasCustomGrip(data: GunData): Boolean = true
+    override fun hasCustomScope(data: GunData): Boolean = true
+    override fun hasCustomBarrel(data: GunData): Boolean = true
+    override fun canEditAttachments(data: GunData): Boolean = true
 }
 
 
