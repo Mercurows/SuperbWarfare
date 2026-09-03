4daba58dc65d1549e4e2def933e013e2571e439c
Atsuishio <842960157@qq.com>
2026-09-03 15:45:08 +0800

    修改定位点骨骼名称

--- src/main/kotlin/com/atsuishio/superbwarfare/item/gun/rifle/MarlinItem.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/gun/rifle/MarlinItem.kt
@@ -4,7 +4,6 @@ import com.atsuishio.superbwarfare.data.gun.GunData
 import com.atsuishio.superbwarfare.item.gun.GeoGunItemV2
 
 object MarlinItem : GeoGunItemV2(Properties()) {
-    override fun hasCustomGrip(data: GunData): Boolean = true
     override fun hasCustomScope(data: GunData): Boolean = true
     override fun hasCustomBarrel(data: GunData): Boolean = true
     override fun canEditAttachments(data: GunData): Boolean = true
