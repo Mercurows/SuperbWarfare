dd85bfd14f39597438f5feac87d84740bde29409
17146 <1714673995@qq.com>
2026-09-05 00:23:33 +0800

    添加acog属性，调整其他配件等级

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
@@ -641,6 +641,7 @@ object ModItems {
     // Scope / sight
     // @formatter:off
     @JvmField val SCOPE_COYOTE = registerAttachment("scope_coyote")
+    @JvmField val SCOPE_ACOG = registerAttachment("scope_acog", Rarity.RARE)
     // @formatter:on
 
     // Grip
