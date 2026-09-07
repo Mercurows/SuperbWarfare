c12c3fd426c015b94e99cc8b800208064a64cb0c
17146 <1714673995@qq.com>
2026-09-07 23:42:49 +0800

    补充切换瞄具（倍率）的音效

--- src/main/kotlin/com/atsuishio/superbwarfare/network/message/send/SwitchScopeMessage.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/network/message/send/SwitchScopeMessage.kt
@@ -3,10 +3,12 @@ package com.atsuishio.superbwarfare.network.message.send
 import com.atsuishio.superbwarfare.data.attachment.AttachmentDefinition
 import com.atsuishio.superbwarfare.data.gun.GunData.Companion.from
 import com.atsuishio.superbwarfare.data.gun.value.AttachmentType
+import com.atsuishio.superbwarfare.init.ModSounds
 import com.atsuishio.superbwarfare.item.gun.GunItem
 import com.atsuishio.superbwarfare.ksp.annotation.RegisterPacket
 import com.atsuishio.superbwarfare.network.PayloadContext
 import com.atsuishio.superbwarfare.network.ServerPacketPayload
+import com.atsuishio.superbwarfare.tools.SoundTool
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -28,5 +30,6 @@ data class SwitchScopeMessage(val scroll: Double) : ServerPacketPayload() {
             tag.putBoolean("ScopeAlt", !tag.getBoolean("ScopeAlt"))
         }
         data.save()
+        SoundTool.playLocalSound(player, ModSounds.ADJUST_FOV.get(), 1f, 0.7f)
     }
 }
