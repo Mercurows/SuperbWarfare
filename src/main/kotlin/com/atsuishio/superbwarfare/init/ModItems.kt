ffa4a5b6dc3eb549b9088209a9a79fd1df89298a
Atsuishio <842960157@qq.com>
2026-09-11 00:22:07 +0800

    添加AimPointT2瞄准镜

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
@@ -655,6 +655,7 @@ object ModItems {
     // Scope / sight
     // @formatter:off
     @JvmField val SCOPE_COYOTE = registerAttachment("scope_coyote")
+    @JvmField val SCOPE_AIMPOINT_T2 = registerAttachment("scope_aimpoint_t2")
     @JvmField val SCOPE_HANDGUN_RED_DOT = registerAttachment("scope_handgun_red_dot")
     @JvmField val SCOPE_EOTECH_551 = registerAttachment("scope_eotech_551")
     @JvmField val SCOPE_OKP_7 = registerAttachment("scope_okp_7")
