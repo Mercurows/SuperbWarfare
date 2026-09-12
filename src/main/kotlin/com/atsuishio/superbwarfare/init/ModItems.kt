2680dfae7f83bdd489530003636845d1c1fd3b2a
Atsuishio <842960157@qq.com>
2026-09-12 21:49:34 +0800

    添加PK-A瞄准镜

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
@@ -668,6 +668,7 @@ object ModItems {
     @JvmField val SCOPE_BRUISER = registerAttachment("scope_bruiser", Rarity.RARE)
     @JvmField val SCOPE_ACOG = registerAttachment("scope_acog", Rarity.RARE)
     @JvmField val SCOPE_1P_78 = registerAttachment("scope_1p_78", Rarity.RARE)
+    @JvmField val SCOPE_PK_A = registerAttachment("scope_pk_a", Rarity.RARE)
     @JvmField val SCOPE_RANGER = registerAttachment("scope_ranger", Rarity.EPIC)
     @JvmField val SCOPE_QMK171 = registerAttachment("scope_qmk171", Rarity.EPIC)
     @JvmField val SCOPE_HAMR = registerAttachment("scope_hamr", Rarity.EPIC)
