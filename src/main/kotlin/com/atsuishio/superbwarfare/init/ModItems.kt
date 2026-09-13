40dbeb67313ae1dbcfe50f168fe3693d66645e42
Atsuishio <842960157@qq.com>
2026-09-13 15:30:52 +0800

    添加卓越-12霰弹消音器

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
@@ -650,6 +650,7 @@ object ModItems {
     @JvmField val HISSILENCER = registerAttachment("hissilencer", Rarity.RARE)
     @JvmField val SILAOWUNCER = registerAttachment("silaowuncer", Rarity.RARE)
     @JvmField val RU_SILENCER = registerAttachment("ru_silencer")
+    @JvmField val SILENCER_SUPERB_12 = registerAttachment("silencer_superb_12")
     @JvmField val SILENCER_50_CAL = registerAttachment("silencer_50_cal")
     @JvmField val AR_SILENCER = registerAttachment("ar_silencer")
     @JvmField val HANDGUN_SILENCER = registerAttachment("handgun_silencer")
