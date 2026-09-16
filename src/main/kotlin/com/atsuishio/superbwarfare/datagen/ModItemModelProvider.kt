037e774a803b44135a0f01c1afaeae8e5b6e1979
Atsuishio <842960157@qq.com>
2026-09-17 00:57:23 +0800

    迁移M79木留弓单发身寸器，重做动画

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -42,7 +42,7 @@ class ModItemModelProvider(output: PackOutput, existingFileHelper: ExistingFileH
         gunItemV2(ModItems.K_98)
         gunItemV2(ModItems.M_4)
         gunItem(ModItems.M_60)
-        gunItem(ModItems.M_79)
+        gunItemV2(ModItems.M_79)
         gunItemV2(ModItems.M_1911)
         gunItemV2(ModItems.M_870)
         gunItem(ModItems.M_98B)
