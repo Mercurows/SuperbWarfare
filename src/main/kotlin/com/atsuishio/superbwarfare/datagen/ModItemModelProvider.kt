494a60f05bc784597525ae7cceb667788a2523bf
Atsuishio <842960157@qq.com>
2026-09-23 12:46:10 +0800

    添加望远镜瞄准镜

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -361,6 +361,7 @@ class ModItemModelProvider(output: PackOutput, existingFileHelper: ExistingFileH
         simpleItem(ModItems.SCOPE_HAMR)
         simpleItem(ModItems.SCOPE_ELCAN_4X)
         simpleItem(ModItems.SCOPE_1P_78)
+        simpleItem(ModItems.SCOPE_SPYGLASS)
         simpleItem(ModItems.SCOPE_PSO_1)
         simpleItem(ModItems.SCOPE_PU)
         simpleItem(ModItems.SCOPE_PK_A)
